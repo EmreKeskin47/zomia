03c168296035b399d67771b7ff6f61501acb2f0b
EmreKeskin47 <43786018+EmreKeskin47@users.noreply.github.com>
2022-04-07 22:55:46 +0300

    bio implemented

---
+++ pages/bio/bio.js
@@ -0,0 +1,60 @@
+export const team = `
+
+Mosaab Alatassi - Logistics Director
+
+Mosaab is a humanitarian development specialist, researcher, and interpreter. He focuses on healthcare issues, especially polio and vaccination coverage in Syria, Yemen and Iraq. He has worked with journalists and academic researchers investigating the Syrian war and Middle East politics. Mosaab is a consultant for the Centre for Humanitarian Dialogue.
+
+Sari Alatassi - Graphic Designer and Interpreter
+
+Sari is a marketing specialist, graphic designer and interpreter. His works focus on the Syrian war, he helps design Zomia publications, and oversees Zomia’s translation team. 
+
+Ahmad Al-Faraj - Field Researcher 
+
+Ahmad is an independent Syrian researcher and journalist. He has worked extensively with internally displaced persons. He holds a bachelor's degree in Arabic literature from the University of Aleppo.
+
+Jeremy Hodge - Senior Investigator 
+
+Jeremy is an investigative journalist and researcher covering the rise of extremism in Syria and Iraq, the petroleum/defense sectors, and regional finance. His 2015 African Confidential investigative report "Opening the Black Box of Egypt's Slush Funds", exposed a $9.4bn network of corruption amongst the country's top generals. Jeremy’s articles have appeared in Foreign Affairs, The Nation, Le Monde Diplomatique, Al-Jazeera, The Daily Beast, Africa Confidential and other outlets.
+
+Kristin MacDougall-Sullivan, esq. - Afghanistan Humanitarian Programs Director 
+
+Kristin is a US-licensed lawyer who runs Zomia’s humanitarian programs in Afghanistan. Prior to joining Zomia, Kristin provided legal counseling and advocacy for asylum seekers in Lesvos, Greece, which at one time hosted the largest refugee camp in Europe. Kristin earned her J.D. from the University of Connecticut School of Law. She also holds a Masters of Arts in Law and Diplomacy from The Fletcher School of Tufts University, where she focused on public international law and human security.  
+
+Hussein Nasser - Field Researcher 
+
+Hussein Nasser is a fellow at the Zomia Center, where he specializes in analyzing the ideologies and formation of terrorist organizations and jihadist groups in Syria. Hussein is the former founder and general manager for the Syrian Counter Extremism Center (SCEC) which led rehabilitation efforts of former jihadists in northern Syria. He received his B.A. in International Relations from Artuklu University in Mardin, Turkey.
+
+Mohamad Yusuf Othman - Country Director - Syria
+
+Mohammad has been working with Zomia since 2017. He directs Zomia projects in Syria, and also supports the Centre for Humanitarian Dialogue. Othman previously worked for Adam Smith International. He holds a bachelor's degree in business management.
+
+Tom A. Peter - Director of Operations
+
+Tom has worked with Zomia since its inception. He oversees all humanitarian projects and administrative functions at Zomia. With over 15 years of experience in the Middle East, Tom worked primarily as a journalist throughout the region with a focus on the conflicts in Iraq, Afghanistan, and Syria before transitioning to the humanitarian sector. A graduate of Northwestern University, Tom also holds a master's degree from the University of Cambridge. 
+
+Hussien Mohammad Samawi - Field Researcher & Medical Coordinator
+
+Hussein is a field researcher and journalist who has done extensive work in Manbij and its countryside, as well as other locations in eastern Syria. He also helps supervise Zomia public health projects, including Leishmanaisis and COVID-19 efforts. 
+`;
+
+export const directors = `
+Anand Gopal
+
+Anand Gopal is an assistant research professor with the Center on the Future of War and the Center for the Study of Religion and Conflict at Arizona State University. He is a journalist and a sociologist who has worked extensively in Afghanistan, Syria and Iraq. He is a writer for The New Yorker. He is the author of No Good Men Among the Living: America, the Taliban and the War Through Afghan Eyes. He received his PhD from Columbia University, and is a fellow at the Type Media Center. 
+
+Nick McDonell
+
+Nick McDonell is a writer of fiction, journalism, and political theory. He studied literature at Harvard University and International Relations at Oxford University. He is author of the novels Twelve and An Expensive Education; his nonfiction works include The Civilization of Perpetual Movement: Nomads in the Modern World and The End of Major Combat Operations. His latest book is The Bodies In Person: An Account of Civilian Casualties in American Wars.
+
+Tom A. Peter 
+
+Tom has worked with Zomia since its inception. He oversees all humanitarian projects and administrative functions at Zomia. With over 15 years of experience in the Middle East, Tom worked primarily as a journalist throughout the region with a focus on the conflicts in Iraq, Afghanistan, and Syria before transitioning to the humanitarian sector. A graduate of Northwestern University, Tom also holds a master's degree from the University of Cambridge. 
+
+Daniel Rothenberg
+
+Daniel Rothenberg is Professor of Practice, School of Politics and Global Studies at Ariona State University and a Senior Fellow at New America. Rothenberg has designed and managed rule of law and human rights projects in Afghanistan, Iraq, Central Africa and throughout Latin America, including programs to train human rights NGOs, aid indigenous peoples in using international legal remedies, support gender justice, and collect and analyze thousands of first-person narratives from victims of atrocities. His books include With These Hands (University of California Press), Memory of Silence: The Guatemalan Truth Commission Report (Palgrave), Drone Wars: Transforming Conflict, Law, and Policy (Cambridge University Press).
+
+Anita Sreedhar
+
+Anita Sreedhar MD MPH is a public health specialist and physician resident at the Montefiore Medical Center in the Bronx, NY. She has focused on the neoliberalization of global public health, such as the rise of contract research organizations (CROs) in Latin America, and cultural and political barriers to vaccination coverage in Afghanistan. She has over a decade of experience as a journalist with NBC News and the Hindustan Times, and received her MPH from Columbia University.
+`;
--- pages/bio/index.js
+++ pages/bio/index.js
@@ -1,10 +1,100 @@
 import React from "react";
+import { Box, Grid, Paper } from "@mui/material";
+import { team, directors } from "./bio";
+import { MakeArticleParagraph2 } from "../../utils/ArticleParagraph";
+import Image from "next/image";
+import RenderTitle from "../../components/RenderTitle";
+import RenderBody from "../../components/RenderBody";
+import RenderBulletPoint from "../../components/RenderBulletPoint";
+import RenderSubtitle from "../../components/RenderSubtitle";
 
 const Bio = () => {
+    const teamData = MakeArticleParagraph2(team);
+    const directorsData = MakeArticleParagraph2(directors);
+
     return (
-        <div>
-            <h1> Bio Page </h1>
-        </div>
+        <Paper
+            sx={{
+                marginTop: { xs: 5, md: 10 },
+            }}
+        >
+            <Grid
+                container
+                justifyItems={"center"}
+                justifySelf={"center"}
+                justifyContent={"center"}
+                alignItems={"center"}
+            >
+                <Grid item xs={10} lg={9} alignSelf={"center"}>
+                    <Box marginY={5}>
+                        <RenderTitle text="Team" />
+                        <Grid
+                            item
+                            xs={12}
+                            lg={10}
+                            justifyItems={"center"}
+                            justifyContent={"center"}
+                            alignItems={"center"}
+                            justifySelf={"center"}
+                        >
+                            {teamData
+                                .split("<br />")
+                                .map((paragraph, index) => {
+                                    if (paragraph.includes(".")) {
+                                        return (
+                                            <RenderBody
+                                                key={index}
+                                                text={paragraph}
+                                            />
+                                        );
+                                    } else {
+                                        return (
+                                            <RenderSubtitle
+                                                key={index}
+                                                text={paragraph}
+                                            />
+                                        );
+                                    }
+                                })}
+                        </Grid>
+
+                        <RenderTitle text="Board Of Directors" />
+                        <Grid
+                            item
+                            xs={12}
+                            lg={10}
+                            justifyItems={"center"}
+                            justifyContent={"center"}
+                            alignItems={"center"}
+                            justifySelf={"center"}
+                        >
+                            {directorsData
+                                .split("<br />")
+                                .map((paragraph, index) => {
+                                    if (
+                                        paragraph.includes(".") &&
+                                        !paragraph.includes("A.")
+                                    ) {
+                                        return (
+                                            <RenderBody
+                                                key={index}
+                                                text={paragraph}
+                                            />
+                                        );
+                                    } else {
+                                        return (
+                                            <RenderSubtitle
+                                                key={index}
+                                                text={paragraph}
+                                            />
+                                        );
+                                    }
+                                })}
+                        </Grid>
+                    </Box>
+                </Grid>
+            </Grid>
+        </Paper>
     );
 };
 

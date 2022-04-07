5c28685ef388f8e70e2a00040d158f28237d3c8a
EmreKeskin47 <43786018+EmreKeskin47@users.noreply.github.com>
2022-04-07 19:22:11 +0300

    analysis page

---
+++ models/Article.js
@@ -0,0 +1,26 @@
+//TODO
+//Each of the 4 article sections will contain a main article photo, date, author name, and short description followed by a “read more” button
+
+export class Article {
+    constructor(
+        id,
+        title,
+        image,
+        date,
+        author,
+        category,
+        description,
+        link,
+        text
+    ) {
+        this.id = id;
+        this.title = title;
+        this.image = image;
+        this.date = date;
+        this.author = author;
+        this.category = category;
+        this.description = description;
+        this.link = link;
+        this.text = text;
+    }
+}

bc661a01d06d1a1e9433534da301d5e8bd6cf951
Simon Arda Yuvarlak <simonyuvarlak@icloud.com>
2022-04-08 13:06:28 +0300

    half baked footer

--- components/VerticalDivider.js
+++ components/VerticalDivider.js
@@ -2,21 +2,21 @@ import React from "react";
 import Divider from "@mui/material/Divider";
 
 const VerticalDivider = () => {
-    return (
-        <Divider
-            orientation="vertical"
-            variant="fullWidth"
-            sx={{
-                minHeight: "200px",
-                borderRightWidth: 8,
-                borderLeftWidth: 8,
-                height: "100%",
-                borderRadius: 8,
-                width: 10,
-                background: "#e5c15e",
-                marginRight: 10,
-            }}
-        ></Divider>
-    );
+  return (
+    <Divider
+      orientation="vertical"
+      variant="fullWidth"
+      sx={{
+        minHeight: "200px",
+        borderRightWidth: 8,
+        borderLeftWidth: 8,
+        height: "100%",
+        borderRadius: 8,
+        width: 10,
+        background: "#e5c15e",
+        marginRight: 10,
+      }}
+    ></Divider>
+  );
 };
 export default VerticalDivider;
--- pages/components/Footer.jsx
+++ pages/components/Footer.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import palette from "../../theme/palette";
-import { Grid, Typography, Box, TextField } from "@mui/material";
-import ArrowRightIcon from "@mui/icons-material/ArrowRight";
+import { Grid, Typography, Box, TextField, Button } from "@mui/material";
 import Input from "@mui/material/Input";
 import { Divider } from "@mui/material";
 import ArticleIcon from "@mui/icons-material/Article";
 import LocalPostOfficeIcon from "@mui/icons-material/LocalPostOffice";
 import SectionBreaker from "./SectionBreaker";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import SendIcon from "@mui/icons-material/Send";
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
+import styled from "@emotion/styled";
 
 const Footer = () => {
+  const StyledButton = styled(Button)(({ theme }) => ({
+    color: "#000",
+    "&:hover": {
+      color: "white",
+      backgroundColor: "transparent",
+    },
+  }));
+
   return (
     <Grid
       container
@@ -17,27 +28,48 @@ const Footer = () => {
       bgcolor={palette.orange.main}
       sx={{ padding: "1rem" }}
     >
-      <Grid item>
-        <Grid container direction="column" justifyContent={"space-around"}>
-          <Grid item>
-            <Grid container direction={"row"} justifyContent="space-evenly">
-              <Grid item>
-                <Typography variant="h6" color={palette.black.main}>
-                  Stay Up To Date !
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Input placeHolder="Email" />
-              </Grid>
+      <Grid item sx={{ padding: "1rem" }}>
+        <StyledButton>
+          <Grid container direction={"row"}>
+            <Grid item sx={{ paddingRight: "0.5rem" }}>
+              <SendIcon />
+            </Grid>
+            <Grid item>
+              <Typography variant="subtitle1" component="div">
+                Send Email
+              </Typography>
             </Grid>
           </Grid>
-        </Grid>
+        </StyledButton>
       </Grid>
-      {/* Second Column */}
+      <Grid item sx={{ padding: "1rem" }}>
+        <StyledButton>
+          <Grid container direction={"row"}>
+            <Grid item sx={{ paddingRight: "0.5rem" }}>
+              <TwitterIcon />
+            </Grid>
+            <Grid item>
+              <Typography variant="subtitle1" component="div">
+                Follow us on Twitter
+              </Typography>
+            </Grid>
+          </Grid>
+        </StyledButton>
+      </Grid>
       <Grid item>
-        <Divider flexItem orientation="vertical" color={"black"}>
-          <ArticleIcon />
-        </Divider>
+        <Divider
+          orientation="vertical"
+          variant="fullWidth"
+          sx={{
+            minHeight: "20px",
+            borderRightWidth: 8,
+            borderLeftWidth: 8,
+            height: "100%",
+            width: 10,
+            background: palette.black.main,
+            borderRadius: 6,
+          }}
+        ></Divider>
       </Grid>
       {/* Third Column */}
       <Grid item>
@@ -64,6 +96,13 @@ const Footer = () => {
                     variant="standard"
                   />
                 </Box>
+              </Grid>
+              <Grid item sx={{ padding: "1rem", border: "2px #000" }}>
+                <StyledButton>
+                  <Typography variant="subtitle1" component="div">
+                    Sign Up
+                  </Typography>
+                </StyledButton>
               </Grid>
             </Grid>
           </Grid>

7461357a129df383eec4169d48fd81da65425488
Simon Arda Yuvarlak <simonyuvarlak@icloud.com>
2022-04-15 17:19:48 +0300

    header ui update

--- pages/analysis/index.js
+++ pages/analysis/index.js
@@ -5,27 +5,32 @@ import { mockArticles } from "../../MOCK_DATA";
 import { Container } from "@mui/material";
 
 const Analysis = () => {
-    return (
-        <Paper sx={{ paddingTop: 5 }}>
-            <Container>
-                <Typography variant="h2" marginY={15} textAlign="center">
-                    Articles
-                </Typography>
-                {mockArticles.map((article, index) => {
-                    return (
-                        <Link key={index} href={`/analysis/${article.id}`}>
-                            <ListView
-                                heading={article.title}
-                                date={article.date}
-                                author={article.author}
-                                image={article.image}
-                            />
-                        </Link>
-                    );
-                })}
-            </Container>
-        </Paper>
-    );
+  return (
+    <Paper sx={{ paddingTop: 5 }}>
+      <Container>
+        <Typography
+          variant="h2"
+          marginY={15}
+          textAlign="center"
+          paddingBottom={10}
+        >
+          Articles
+        </Typography>
+        {mockArticles.map((article, index) => {
+          return (
+            <Link key={index} href={`/analysis/${article.id}`}>
+              <ListView
+                heading={article.title}
+                date={article.date}
+                author={article.author}
+                image={article.image}
+              />
+            </Link>
+          );
+        })}
+      </Container>
+    </Paper>
+  );
 };
 
 export default Analysis;
--- pages/investigations/index.js
+++ pages/investigations/index.js
@@ -5,27 +5,32 @@ import { Container } from "@mui/material";
 import { mockReports } from "../../MOCK_PDF";
 
 const Investigations = () => {
-    return (
-        <Paper sx={{ paddingTop: 5 }}>
-            <Container>
-                <Typography variant="h2" marginY={15} textAlign="center">
-                    Reports
-                </Typography>
-                {mockReports.map((report, index) => {
-                    return (
-                        <Link key={index} href={`/investigations/${report.id}`}>
-                            <ListView
-                                heading={report.title}
-                                date={report.date}
-                                author={report.author}
-                                image={report.image}
-                            />
-                        </Link>
-                    );
-                })}
-            </Container>
-        </Paper>
-    );
+  return (
+    <Paper sx={{ paddingTop: 5 }}>
+      <Container>
+        <Typography
+          variant="h2"
+          marginY={15}
+          textAlign="center"
+          paddingBottom={10}
+        >
+          Reports
+        </Typography>
+        {mockReports.map((report, index) => {
+          return (
+            <Link key={index} href={`/investigations/${report.id}`}>
+              <ListView
+                heading={report.title}
+                date={report.date}
+                author={report.author}
+                image={report.image}
+              />
+            </Link>
+          );
+        })}
+      </Container>
+    </Paper>
+  );
 };
 
 export default Investigations;

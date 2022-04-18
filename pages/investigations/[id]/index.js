3dddeb25c4828130552941190d87f3003996157b
Simon Arda Yuvarlak <simonyuvarlak@icloud.com>
2022-04-18 23:57:52 +0300

    ui update

--- pages/investigations/[id]/index.js
+++ pages/investigations/[id]/index.js
@@ -4,59 +4,69 @@ import Paper from "@mui/material/Paper";
 import Article from "../../../components/Article";
 import Link from "next/link";
 import { Box, Typography } from "@mui/material";
+import styled from "@emotion/styled";
+import palette from "../../../theme/palette";
 
 const Investigations = (props) => {
-    const { id } = props;
-    const [report, setReport] = useState(null);
+  const { id } = props;
+  const [report, setReport] = useState(null);
 
-    useEffect(() => {
-        setReport(mockReports[id - 1]);
-    }, [id]);
+  useEffect(() => {
+    setReport(mockReports[id - 1]);
+  }, [id]);
 
-    return (
-        <Paper sx={{ paddingTop: 5 }}>
-            {report && <Article article={report} isReport={true} />}
-            {report && (
-                <Link href={"/" + report.pdfLink} replace>
-                    <Box sx={{ paddingY: 10 }}>
-                        <Typography
-                            variant="h5"
-                            textAlign={"center"}
-                            sx={{
-                                textDecoration: "none",
-                                color: "whitesmoke",
-                                fontSize: 20,
-                                cursor: "pointer",
-                            }}
-                        >
-                            Click to view the full report
-                        </Typography>
-                    </Box>
-                </Link>
-            )}
-        </Paper>
-    );
+  const StyledTypography = styled(Typography)(({ theme }) => ({
+    color: "whitesmoke",
+    "&:hover": {
+      color: theme.palette.orange.main,
+      backgroundColor: "transparent",
+    },
+  }));
+
+  return (
+    <Paper sx={{ paddingTop: 5 }}>
+      {report && <Article article={report} isReport={true} />}
+      {report && (
+        <Link href={"/" + report.pdfLink} replace>
+          <Box sx={{ paddingY: 10 }}>
+            <StyledTypography
+              variant="h5"
+              textAlign={"center"}
+              sx={{
+                textDecoration: "none",
+                color: "whitesmoke",
+                fontSize: 20,
+                cursor: "pointer",
+              }}
+            >
+              Click to view the full report
+            </StyledTypography>
+          </Box>
+        </Link>
+      )}
+    </Paper>
+  );
 };
 
 export default Investigations;
 
 export async function getStaticProps({ params }) {
-    const id = params.id;
-    return {
-        props: {
-            id,
-        },
-    };
+  const id = params.id;
+  return {
+    props: {
+      id,
+    },
+  };
 }
 
 export async function getStaticPaths() {
-    return {
-        paths: [
-            { params: { id: "1" } },
-            { params: { id: "2" } },
-            { params: { id: "3" } },
-            { params: { id: "4" } },
-        ],
-        fallback: false,
-    };
+  return {
+    paths: [
+      { params: { id: "1" } },
+      { params: { id: "2" } },
+      { params: { id: "3" } },
+      { params: { id: "4" } },
+    ],
+    fallback: false,
+  };
 }

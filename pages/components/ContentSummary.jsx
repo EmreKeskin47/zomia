854192998b477c34835a9b07c6b46fb2d11f13b1
Simon Arda Yuvarlak <simonyuvarlak@icloud.com>
2022-04-18 23:44:39 +0300

    delete category

--- pages/components/ContentSummary.jsx
+++ pages/components/ContentSummary.jsx
@@ -2,51 +2,41 @@ import { Grid, Typography, Link } from "@mui/material";
 import React from "react";
 
 const ContentSummary = ({ id, category, heading, date, author }) => {
-    return (
-        <Grid container direction="column" justifyContent="center">
-            <Grid item sx={{ paddingBottom: "0.3rem" }}>
-                <Link
-                    underline="none"
-                    color="whitesmoke"
-                    align="left"
-                    variant="overline"
-                >
-                    {category && category.toUpperCase()}
-                </Link>
-            </Grid>
-            <Grid item sx={{ paddingBottom: "0.5rem" }}>
-                <Link
-                    href={`/investigations/${id}`}
-                    underline="none"
-                    color="whitesmoke"
-                    align="left"
-                    variant="h6"
-                    sx={{ fontWeight: "bold" }}
-                >
-                    {heading}
-                </Link>
-            </Grid>
-            <Grid item>
-                <Typography
-                    align="left"
-                    variant="caption"
-                    sx={{ fontStyle: "italic", color: "whitesmoke" }}
-                >
-                    {"on " + date}
-                </Typography>
-            </Grid>
-            <Grid item sx={{ paddingBottom: "1rem" }}>
-                <Typography
-                    align="left"
-                    variant="caption"
-                    sx={{ fontStyle: "italic", color: "whitesmoke" }}
-                >
-                    {"by " + author}
-                </Typography>
-            </Grid>
-            <hr />
-        </Grid>
-    );
+  return (
+    <Grid container direction="column" justifyContent="center">
+      <Grid item sx={{ paddingBottom: "0.5rem" }}>
+        <Link
+          href={`/investigations/${id}`}
+          underline="none"
+          color="whitesmoke"
+          align="left"
+          variant="h6"
+          sx={{ fontWeight: "bold" }}
+        >
+          {heading}
+        </Link>
+      </Grid>
+      <Grid item>
+        <Typography
+          align="left"
+          variant="caption"
+          sx={{ fontStyle: "italic", color: "whitesmoke" }}
+        >
+          {"on " + date}
+        </Typography>
+      </Grid>
+      <Grid item sx={{ paddingBottom: "1rem" }}>
+        <Typography
+          align="left"
+          variant="caption"
+          sx={{ fontStyle: "italic", color: "whitesmoke" }}
+        >
+          {"by " + author}
+        </Typography>
+      </Grid>
+      <hr />
+    </Grid>
+  );
 };
 
 export default ContentSummary;
--- pages/components/IntroContentSummaries.jsx
+++ pages/components/IntroContentSummaries.jsx
@@ -4,23 +4,24 @@ import { ContentSummaryArray } from "../../components/data/ContentSumaryArray";
 import { Grid } from "@mui/material";
 
 const IntroContentSummaries = () => {
-    return (
-        <Grid>
-            {ContentSummaryArray.map((contentSummary, index) => {
-                return (
-                    <Grid sx={{ padding: "0.5rem" }} key={index}>
-                        <ContentSummary
-                            id={contentSummary.id}
-                            category={contentSummary.category}
-                            heading={contentSummary.heading}
-                            date={contentSummary.date}
-                            author={contentSummary.author}
-                        />
-                    </Grid>
-                );
-            })}
-        </Grid>
-    );
+  return (
+    <Grid>
+      {ContentSummaryArray.map((contentSummary, index) => {
+        return (
+          <Grid sx={{ padding: "0.5rem" }} key={index}>
+            <ContentSummary
+              id={contentSummary.id}
+              category={contentSummary.category}
+              heading={contentSummary.heading}
+              date={contentSummary.date}
+              author={contentSummary.author}
+              style={{ paddingBottom: "5rem" }}
+            />
+          </Grid>
+        );
+      })}
+    </Grid>
+  );
 };
 
 export default IntroContentSummaries;

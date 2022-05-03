ce5e4ad4db7d95bd636d0da7d7b1cc90bc3d5020
Simon Arda Yuvarlak <simonyuvarlak@icloud.com>
2022-05-03 16:20:42 +0300

    mobile dark mode toggle

--- layout/MobileBar.js
+++ layout/MobileBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import List from "@mui/material/List";
@@ -11,104 +11,120 @@ import ListItemText from "@mui/material/ListItemText";
 import AppBar from "@mui/material/AppBar";
 import Drawer from "@mui/material/Drawer";
 import imageLoader from "../loader";
+import singleContext from "../SingleContext";
+import Checkbox from "@mui/material/Checkbox";
+import NightsStayIcon from "@mui/icons-material/NightsStay";
+import WbSunnyIcon from "@mui/icons-material/WbSunny";
 
 const MobileBar = () => {
-    const [drawer, setDrawer] = useState(false);
-    const toggleDrawer = () => (event) => {
-        if (
-            event.type === "keydown" &&
-            (event.key === "Tab" || event.key === "Shift")
-        ) {
-            return;
-        }
+  const context = useContext(singleContext);
+  const [drawer, setDrawer] = useState(false);
+  const toggleDrawer = () => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
 
-        setDrawer(!drawer);
-    };
+    setDrawer(!drawer);
+  };
 
-    const drawerItemList = () => (
+  const drawerItemList = () => (
+    <Box
+      role="presentation"
+      onClick={toggleDrawer()}
+      onKeyDown={toggleDrawer()}
+      width={250}
+    >
+      <List>
+        <Divider sx={{ paddingBottom: 5, marginTop: 10 }} />
+        <ListItem button sx={{ marginY: 2 }}>
+          <Link href="/">
+            <ListItemText primary="Home" margin={10} />
+          </Link>
+        </ListItem>
+
+        <ListItem button>
+          <Link href="/admin">
+            <ListItemText primary="Admin" />
+          </Link>
+        </ListItem>
+        <ListItem button sx={{ marginY: 2 }}>
+          <Link href="/analysis">
+            <ListItemText primary="Analysis" />
+          </Link>
+        </ListItem>
+        <ListItem button>
+          <Link href="/projects">
+            <ListItemText primary="Projects" />
+          </Link>
+        </ListItem>
+        <ListItem button sx={{ marginY: 2 }}>
+          <Link href="/investigations">
+            <ListItemText primary="Investigations" />
+          </Link>
+        </ListItem>
+        <ListItem button>
+          <Link href="/mission">
+            <ListItemText primary="Mission" />
+          </Link>
+        </ListItem>
+        <ListItem button sx={{ marginY: 2 }}>
+          <Link href="/bio">
+            <ListItemText primary="Bio" />
+          </Link>
+        </ListItem>
+        <ListItem button sx={{ marginY: 2 }}>
+          <Checkbox
+            icon={<NightsStayIcon />}
+            checkedIcon={<WbSunnyIcon />}
+            onClick={() => {
+              console.log(context);
+              context.toggleDarkMode();
+              console.log(context);
+            }}
+          />
+        </ListItem>
+      </List>
+      <Divider />
+    </Box>
+  );
+  return (
+    <>
+      <Drawer anchor={"left"} open={drawer} onClose={toggleDrawer()}>
+        {drawerItemList()}
+      </Drawer>
+      <AppBar>
         <Box
-            role="presentation"
-            onClick={toggleDrawer()}
-            onKeyDown={toggleDrawer()}
-            width={250}
+          width={"100%"}
+          display={"flex"}
+          justifyContent={"space-between"}
+          sx={{ cursor: "pointer" }}
         >
-            <List>
-                <Divider sx={{ paddingBottom: 5, marginTop: 10 }} />
-                <ListItem button sx={{ marginY: 2 }}>
-                    <Link href="/">
-                        <ListItemText primary="Home" margin={10} />
-                    </Link>
-                </ListItem>
-
-                <ListItem button>
-                    <Link href="/admin">
-                        <ListItemText primary="Admin" />
-                    </Link>
-                </ListItem>
-                <ListItem button sx={{ marginY: 2 }}>
-                    <Link href="/analysis">
-                        <ListItemText primary="Analysis" />
-                    </Link>
-                </ListItem>
-                <ListItem button>
-                    <Link href="/projects">
-                        <ListItemText primary="Projects" />
-                    </Link>
-                </ListItem>
-                <ListItem button sx={{ marginY: 2 }}>
-                    <Link href="/investigations">
-                        <ListItemText primary="Investigations" />
-                    </Link>
-                </ListItem>
-                <ListItem button>
-                    <Link href="/mission">
-                        <ListItemText primary="Mission" />
-                    </Link>
-                </ListItem>
-                <ListItem button sx={{ marginY: 2 }}>
-                    <Link href="/bio">
-                        <ListItemText primary="Bio" />
-                    </Link>
-                </ListItem>
-            </List>
-            <Divider />
+          <Link href="/">
+            <Image
+              src="/static/logo.jpeg"
+              loader={imageLoader}
+              unoptimized
+              alt="Logo"
+              width="140px"
+              height="64px"
+            />
+          </Link>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 4 }}
+            onClick={toggleDrawer()}
+          >
+            <MenuIcon />
+          </IconButton>
         </Box>
-    );
-    return (
-        <>
-            <Drawer anchor={"left"} open={drawer} onClose={toggleDrawer()}>
-                {drawerItemList()}
-            </Drawer>
-            <AppBar>
-                <Box
-                    width={"100%"}
-                    display={"flex"}
-                    justifyContent={"space-between"}
-                    sx={{ cursor: "pointer" }}
-                >
-                    <Link href="/">
-                        <Image
-                            src="/static/logo.jpeg"
-                            loader={imageLoader}
-                            unoptimized
-                            alt="Logo"
-                            width="140px"
-                            height="64px"
-                        />
-                    </Link>
-                    <IconButton
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 4 }}
-                        onClick={toggleDrawer()}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                </Box>
-            </AppBar>
-        </>
-    );
+      </AppBar>
+    </>
+  );
 };
 
 export default MobileBar;

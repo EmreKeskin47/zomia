74c79a4247732adffcad5987a3f9a32ba8f242a9
Simon Arda Yuvarlak <simonyuvarlak@icloud.com>
2022-05-05 00:21:50 +0300

    limit report number in homepage

--- pages/components/CardList.jsx
+++ pages/components/CardList.jsx
@@ -6,46 +6,36 @@ import { useSelector } from "react-redux";
 import { useReportData } from "../../store/hooks/useData";
 
 const CardList = ({ type }) => {
-    const reportList = useReportData();
-    const generateList = () => {
-        if (type === "report") {
-            return (
-                <Grid
-                    container
-                    direction={"row"}
-                    justifyContent={"space-around"}
+  const reportList = useReportData();
+  const generateList = () => {
+    if (type === "report") {
+      return (
+        <Grid container direction={"row"} justifyContent={"space-around"}>
+          {reportList &&
+            reportList.slice(0, 3).map((item) => (
+              <Grid item key={item.id} sx={{ paddingLeft: 5, paddingRight: 5 }}>
+                <Link
+                  href={`/investigations/${item.id}`}
+                  sx={{ paddingLeft: 5, paddingRight: 5 }}
+                  replace="true"
                 >
-                    {reportList &&
-                        reportList.map((item) => (
-                            <Grid
-                                item
-                                key={item.id}
-                                sx={{ paddingLeft: 5, paddingRight: 5 }}
-                            >
-                                <Link
-                                    href={`/investigations/${item.id}`}
-                                    sx={{ paddingLeft: 5, paddingRight: 5 }}
-                                    replace="true"
-                                >
-                                    <NewsSummary
-                                        image={item.image}
-                                        title={item.title}
-                                        date={item.date}
-                                        description={
-                                            item.text.slice(0, 140) + "..."
-                                        }
-                                        author={item.author}
-                                    />
-                                </Link>
-                            </Grid>
-                        ))}
-                </Grid>
-            );
-        } else {
-            return <Grid>Articles part is under construction.</Grid>;
-        }
-    };
-    return <Grid>{generateList()}</Grid>;
+                  <NewsSummary
+                    image={item.image}
+                    title={item.title}
+                    date={item.date}
+                    description={item.text.slice(0, 140) + "..."}
+                    author={item.author}
+                  />
+                </Link>
+              </Grid>
+            ))}
+        </Grid>
+      );
+    } else {
+      return <Grid>Articles part is under construction.</Grid>;
+    }
+  };
+  return <Grid>{generateList()}</Grid>;
 };
 
 export default CardList;

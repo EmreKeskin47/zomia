9788948631ed5fe3fa880b2bc202e49ab8649e6c
Simon Arda Yuvarlak <simonyuvarlak@icloud.com>
2022-05-31 16:37:51 +0300

    removal of 'on' from dates

--- components/Article.js
+++ components/Article.js
@@ -12,137 +12,114 @@ import SectionBreaker from "../pages/components/SectionBreaker";
 import imageLoader from "../loader";
 
 const Article = (props) => {
-    const { article, isReport } = props;
+  const { article, isReport } = props;
 
-    if (article) {
-        article.text = MakeArticleParagraph2(article.text);
+  if (article) {
+    article.text = MakeArticleParagraph2(article.text);
 
-        return (
-            <>
-                <AppBar position="fixed" sx={{ zIndex: 2 }}>
-                    <OnlyTabsBar />
-                    <AppBar
-                        position="relative"
-                        color="secondary"
-                        sx={{ paddingTop: 0.4, textIndent: "1rem" }}
-                    >
-                        <Typography variant="h6" fontStyle={"italic"}>
-                            {article.title}
+    return (
+      <>
+        <AppBar position="fixed" sx={{ zIndex: 2 }}>
+          <OnlyTabsBar />
+          <AppBar
+            position="relative"
+            color="secondary"
+            sx={{ paddingTop: 0.4, textIndent: "1rem" }}
+          >
+            <Typography variant="h6" fontStyle={"italic"}>
+              {article.title}
+            </Typography>
+          </AppBar>
+        </AppBar>
+
+        <Paper>
+          <Grid
+            container
+            justifyItems={"center"}
+            justifySelf={"center"}
+            justifyContent={"center"}
+            alignItems={"center"}
+          >
+            <Grid item xs={10} lg={9} alignSelf={"center"}>
+              <Box marginY={5}>
+                <Box display="flex" alignItems="center" marginY={5}>
+                  <VerticalDivider />
+                  <Box>
+                    <Typography variant="h3" sx={{ marginY: 3 }}>
+                      {article.title}
+                    </Typography>
+                    {article.author && (
+                      <>
+                        <Typography
+                          variant="subtitle1"
+                          fontStyle={"italic"}
+                          sx={{ marginY: 3 }}
+                        >
+                          {"by " + article.author}
                         </Typography>
-                    </AppBar>
-                </AppBar>
-
-                <Paper>
-                    <Grid
-                        container
-                        justifyItems={"center"}
-                        justifySelf={"center"}
-                        justifyContent={"center"}
-                        alignItems={"center"}
-                    >
-                        <Grid item xs={10} lg={9} alignSelf={"center"}>
-                            <Box marginY={5}>
-                                <Box
-                                    display="flex"
-                                    alignItems="center"
-                                    marginY={5}
-                                >
-                                    <VerticalDivider />
-                                    <Box>
-                                        <Typography
-                                            variant="h3"
-                                            sx={{ marginY: 3 }}
-                                        >
-                                            {article.title}
-                                        </Typography>
-                                        {article.author && (
-                                            <>
-                                                <Typography
-                                                    variant="subtitle1"
-                                                    fontStyle={"italic"}
-                                                    sx={{ marginY: 3 }}
-                                                >
-                                                    {"by " + article.author}
-                                                </Typography>
-                                                <Typography
-                                                    variant="subtitle1"
-                                                    fontStyle={"italic"}
-                                                    sx={{ marginY: 3 }}
-                                                >
-                                                    {"on " + article.date}
-                                                </Typography>
-                                            </>
-                                        )}
-                                    </Box>
-                                </Box>
-
-                                {article.image && (
-                                    <Box>
-                                        <Image
-                                            src={article.image}
-                                            loader={imageLoader}
-                                            unoptimized
-                                            width={"1300px"}
-                                            height={"750px"}
-                                            alt="zomia article"
-                                        />
-                                    </Box>
-                                )}
-
-                                <Grid
-                                    item
-                                    xs={12}
-                                    lg={11.5}
-                                    justifyItems={"center"}
-                                    justifyContent={"center"}
-                                    alignItems={"center"}
-                                    justifySelf={"center"}
-                                >
-                                    {article.text
-                                        .split("<br/>" || "<br>")
-                                        .map((paragraph, index) => {
-                                            if (
-                                                paragraph.includes(".") ||
-                                                paragraph.includes("@")
-                                            ) {
-                                                return (
-                                                    <RenderBody
-                                                        key={index}
-                                                        text={paragraph}
-                                                    />
-                                                );
-                                            } else {
-                                                return (
-                                                    <RenderSubtitle
-                                                        key={index}
-                                                        text={paragraph}
-                                                    />
-                                                );
-                                            }
-                                        })}
-                                </Grid>
-                            </Box>
-                        </Grid>
-                    </Grid>
-                    {!isReport && (
-                        <>
-                            <SectionBreaker
-                                text="Related Articles"
-                                link="/investigations"
-                            />
-                            <CardList type="report" />
-                        </>
+                        <Typography
+                          variant="subtitle1"
+                          fontStyle={"italic"}
+                          sx={{ marginY: 3 }}
+                        >
+                          {article.date}
+                        </Typography>
+                      </>
                     )}
-                </Paper>
+                  </Box>
+                </Box>
+
+                {article.image && (
+                  <Box>
+                    <Image
+                      src={article.image}
+                      loader={imageLoader}
+                      unoptimized
+                      width={"1300px"}
+                      height={"750px"}
+                      alt="zomia article"
+                    />
+                  </Box>
+                )}
+
+                <Grid
+                  item
+                  xs={12}
+                  lg={11.5}
+                  justifyItems={"center"}
+                  justifyContent={"center"}
+                  alignItems={"center"}
+                  justifySelf={"center"}
+                >
+                  {article.text
+                    .split("<br/>" || "<br>")
+                    .map((paragraph, index) => {
+                      if (paragraph.includes(".") || paragraph.includes("@")) {
+                        return <RenderBody key={index} text={paragraph} />;
+                      } else {
+                        return <RenderSubtitle key={index} text={paragraph} />;
+                      }
+                    })}
+                </Grid>
+              </Box>
+            </Grid>
+          </Grid>
+          {!isReport && (
+            <>
+              <SectionBreaker text="Related Articles" link="/investigations" />
+              <CardList type="report" />
             </>
-        );
-    } else {
-        return (
-            <div>
-                <h1>Loading...</h1>
-            </div>
-        );
-    }
+          )}
+        </Paper>
+      </>
+    );
+  } else {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
 };
 
 export default Article;
--- pages/components/ContentSummary.jsx
+++ pages/components/ContentSummary.jsx
@@ -2,48 +2,40 @@ import { Grid, Typography, Link } from "@mui/material";
 import React from "react";
 
 const ContentSummary = ({ id, description, heading, date, author }) => {
-    return (
-        <Grid container direction="column" justifyContent="center">
-            <Grid item sx={{ paddingBottom: "0.5rem" }}>
-                <Link
-                    href={`/analysis/${id}`}
-                    underline="none"
-                    color="whitesmoke"
-                    align="left"
-                    variant="h5"
-                    sx={{ fontWeight: "bold" }}
-                >
-                    {heading}
-                </Link>
-            </Grid>
-            {/* <Grid item>
+  return (
+    <Grid container direction="column" justifyContent="center">
+      <Grid item sx={{ paddingBottom: "0.5rem" }}>
+        <Link
+          href={`/analysis/${id}`}
+          underline="none"
+          color="whitesmoke"
+          align="left"
+          variant="h5"
+          sx={{ fontWeight: "bold" }}
+        >
+          {heading}
+        </Link>
+      </Grid>
+      {/* <Grid item>
         <Typography align="left" variant="h6">
           {description}
         </Typography>
       </Grid> */}
-            <Grid item>
-                <Typography
-                    align="left"
-                    variant="caption"
-                    sx={{ fontStyle: "italic" }}
-                >
-                    {"on " + date}
-                </Typography>
-            </Grid>
-            <Grid item sx={{ paddingBottom: "1rem" }}>
-                <Typography
-                    align="left"
-                    variant="caption"
-                    sx={{ fontStyle: "italic" }}
-                >
-                    {"by " + author}
-                </Typography>
-            </Grid>
-            <Grid sx={{ marginTop: 1.5, marginBottom: 1.5 }}>
-                <hr />
-            </Grid>
-        </Grid>
-    );
+      <Grid item>
+        <Typography align="left" variant="caption" sx={{ fontStyle: "italic" }}>
+          {date}
+        </Typography>
+      </Grid>
+      <Grid item sx={{ paddingBottom: "1rem" }}>
+        <Typography align="left" variant="caption" sx={{ fontStyle: "italic" }}>
+          {"by " + author}
+        </Typography>
+      </Grid>
+      <Grid sx={{ marginTop: 1.5, marginBottom: 1.5 }}>
+        <hr />
+      </Grid>
+    </Grid>
+  );
 };
 
 export default ContentSummary;

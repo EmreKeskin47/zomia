063004a1a7f8bda6ae854bf1bae3edd95c29f21c
Simon Arda Yuvarlak <simonyuvarlak@icloud.com>
2022-06-02 17:25:22 +0300

    navbar logo modification

--- layout/OnlyTabsBar.js
+++ layout/OnlyTabsBar.js
@@ -8,42 +8,40 @@ import singleContext from "../SingleContext";
 import palette from "../theme/palette";
 
 const OnlyTabsBar = () => {
-    const context = useContext(singleContext);
-    return (
-        <Box
-            justifyContent="space-around"
-            display="flex"
-            width={"100%"}
-            height={"100%"}
-            sx={{
-                backgroundColor: context.darkMode
-                    ? palette.black.main
-                    : "whitesmoke",
-            }}
-        >
-            <Box
-                width={"30%"}
-                justifyContent="center"
-                display="flex"
-                alignItems={"center"}
-                sx={{ cursor: "pointer" }}
-            >
-                <Link href="/" passhref="true">
-                    <Image
-                        src="/static/logo.png"
-                        loader={imageLoader}
-                        unoptimized
-                        alt="Logo"
-                        width="80px"
-                        height="64px"
-                    />
-                </Link>
-            </Box>
-            <Box width={"60%"} sx={{ marginRight: "5%", alignSelf: "center" }}>
-                <Tabs />
-            </Box>
-        </Box>
-    );
+  const context = useContext(singleContext);
+  return (
+    <Box
+      justifyContent="space-around"
+      display="flex"
+      width={"100%"}
+      height={"100%"}
+      sx={{
+        backgroundColor: context.darkMode ? palette.black.main : "whitesmoke",
+      }}
+    >
+      <Box
+        width={"30%"}
+        justifyContent="center"
+        display="flex"
+        alignItems={"center"}
+        sx={{ cursor: "pointer" }}
+      >
+        <Link href="/" passhref="true">
+          <Image
+            src="/static/logo.png"
+            loader={imageLoader}
+            unoptimized
+            alt="Logo"
+            width="100px"
+            height="80px"
+          />
+        </Link>
+      </Box>
+      <Box width={"60%"} sx={{ marginRight: "5%", alignSelf: "center" }}>
+        <Tabs />
+      </Box>
+    </Box>
+  );
 };
 
 export default OnlyTabsBar;

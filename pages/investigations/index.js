37447a07557c97db986b25b697a034253896341b
Simon Arda Yuvarlak <simonyuvarlak@icloud.com>
2022-06-19 07:31:46 -0700

    update order in investigations

--- pages/investigations/index.js
+++ pages/investigations/index.js
@@ -8,78 +8,75 @@ import { useReportData } from "../../store/hooks/useData";
 import { Divider } from "@mui/material";
 
 const Investigations = (props) => {
-    const [connectReport, setConnectReport] = useState([]);
+  const [connectReport, setConnectReport] = useState([]);
 
-    useEffect(() => {
-        const fetch = async () => {
-            await props.fetchReports();
-            setConnectReport(props.reports);
-        };
-        fetch();
-    }, [props.fetchReports, props.report]);
+  useEffect(() => {
+    const fetch = async () => {
+      await props.fetchReports();
+      setConnectReport(props.reports);
+    };
+    fetch();
+  }, [props.fetchReports, props.report]);
 
-    const reports = useReportData();
-    return (
-        <Paper sx={{ paddingTop: 5 }}>
-            <Container>
-                <Typography
-                    variant="h2"
-                    marginY={15}
-                    textAlign="center"
-                    paddingBottom={10}
-                    paddingTop={5}
-                    sx={{
-                        fontSize: " 3.75rem !important",
-                        fontFamily: "Tiro Telugu !important",
-                    }}
-                >
-                    Investigations
-                </Typography>
-                <div style={{ paddingBottom: "125px" }}>
-                    {reports &&
-                        reports.map((report, index) => {
-                            return (
-                                <>
-                                    <Link
-                                        key={index}
-                                        href={`/investigations/${report.id}`}
-                                    >
-                                        <ListView
-                                            heading={report.title}
-                                            date={report.date}
-                                            author={report.author}
-                                            image={report.image}
-                                            description={report.description}
-                                        />
-                                    </Link>
-                                    {index < reports.length - 1 && (
-                                        <Divider
-                                            flexItem
-                                            sx={{
-                                                marginBottom: "3rem",
-                                                marginTop: "1rem",
-                                            }}
-                                        ></Divider>
-                                    )}
-                                </>
-                            );
-                        })}
-                </div>
-            </Container>
-        </Paper>
-    );
+  const reports = useReportData();
+  return (
+    <Paper sx={{ paddingTop: 5 }}>
+      <Container>
+        <Typography
+          variant="h2"
+          marginY={15}
+          textAlign="center"
+          paddingBottom={10}
+          paddingTop={5}
+          sx={{
+            fontSize: " 3.75rem !important",
+            fontFamily: "Tiro Telugu !important",
+          }}
+        >
+          Investigations
+        </Typography>
+        <div style={{ paddingBottom: "125px" }}>
+          {reports &&
+            reports.reverse().map((report, index) => {
+              return (
+                <>
+                  <Link key={index} href={`/investigations/${report.id}`}>
+                    <ListView
+                      heading={report.title}
+                      date={report.date}
+                      author={report.author}
+                      image={report.image}
+                      description={report.description}
+                    />
+                  </Link>
+                  {index < reports.length - 1 && (
+                    <Divider
+                      flexItem
+                      sx={{
+                        marginBottom: "3rem",
+                        marginTop: "1rem",
+                      }}
+                    ></Divider>
+                  )}
+                </>
+              );
+            })}
+        </div>
+      </Container>
+    </Paper>
+  );
 };
 
 const mapStateToProps = (state) => {
-    return {
-        reports: state.reportStore.reports,
-    };
+  return {
+    reports: state.reportStore.reports,
+  };
 };
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        fetchReports: () => dispatch(reportActions.fetchReports()),
-    };
+  return {
+    fetchReports: () => dispatch(reportActions.fetchReports()),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Investigations);

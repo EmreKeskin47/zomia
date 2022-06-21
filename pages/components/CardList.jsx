02efd475235f037c5460b4890cbca28f7d3b1238
Simon Arda Yuvarlak <simonyuvarlak@icloud.com>
2022-06-20 17:22:05 -0700

    bug fix

--- pages/components/CardList.jsx
+++ pages/components/CardList.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { Grid, Link } from "@mui/material";
 import NewsSummary from "./NewsSummary";
-import { useReverseReportData } from "../../store/hooks/useData";
+import {
+  useReverseReportData,
+  useArticleData,
+} from "../../store/hooks/useData";
 
 const CardList = ({ type }) => {
-  const reportList = useReverseReportData();
   const generateList = () => {
     if (type === "report") {
+      const reportList = useReverseReportData();
       return (
         <Grid container direction={"row"} justifyContent={"center"}>
           {reportList &&
@@ -41,7 +44,40 @@ const CardList = ({ type }) => {
         </Grid>
       );
     } else {
-      return <Grid>Articles part is under construction.</Grid>;
+      const articleList = useArticleData();
+      return (
+        <Grid container direction={"row"} justifyContent={"center"}>
+          {articleList &&
+            articleList.slice(0, 3).map((item) => (
+              <Grid
+                item
+                key={item.id}
+                sx={{
+                  paddingLeft: 3,
+                  paddingRight: 3,
+                }}
+              >
+                <Link
+                  href={`/investigations/${item.id}`}
+                  sx={{ paddingLeft: 3, paddingRight: 3 }}
+                  replace="true"
+                >
+                  <NewsSummary
+                    image={item.image}
+                    title={item.title}
+                    date={item.date}
+                    description={
+                      item.description
+                        ? item.description.slice(0, 400)
+                        : "No Description"
+                    }
+                    author={item.author}
+                  />
+                </Link>
+              </Grid>
+            ))}
+        </Grid>
+      );
     }
   };
   return <Grid>{generateList()}</Grid>;

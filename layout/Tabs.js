35ab7ebc9a7b25b89a838a3da8acb0cb8a3d085b
Poseidon <ardayuvarlak@outlook.com>
2022-06-24 18:18:36 -0700

    update email

--- layout/Tabs.js
+++ layout/Tabs.js
@@ -15,242 +15,212 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import Divider from "@mui/material/Divider";
 
 const checkBoxStyles = (theme) => ({
-    root: {
-        color: "#F9A21B ",
-        marginRight: "5%",
-    },
+  root: {
+    color: "#F9A21B ",
+    marginRight: "5%",
+  },
 });
 
 const CustomCheckbox = withStyles(checkBoxStyles)(Checkbox);
 
 const Tabs = (props) => {
-    const context = useContext(singleContext);
+  const context = useContext(singleContext);
 
-    const [anchorElUser, setAnchorElUser] = useState(null);
-    const { border } = props;
+  const [anchorElUser, setAnchorElUser] = useState(null);
+  const { border } = props;
 
-    const handleOpenUserMenu = (event) => {
-        setAnchorElUser(event.currentTarget);
-    };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
 
-    const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
-    };
-    return (
-        <Box
-            justifyContent="space-between"
-            display="flex"
-            height={"48px"}
-            alignItems={"center"}
-            sx={{
-                cursor: "pointer",
-                borderTop: border && "1px solid #2c253c",
-                padding: 0,
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+  return (
+    <Box
+      justifyContent="space-between"
+      display="flex"
+      height={"48px"}
+      alignItems={"center"}
+      sx={{
+        cursor: "pointer",
+        borderTop: border && "1px solid #2c253c",
+        padding: 0,
+      }}
+    >
+      <Link href="/" passhref="true">
+        <Typography variant="h6">
+          <span
+            style={{
+              color: context.darkMode ? "#fff" : palette.black.main,
             }}
-        >
-            <Link href="/" passhref="true">
-                <Typography variant="h6">
-                    <span
-                        style={{
-                            color: context.darkMode
-                                ? "#fff"
-                                : palette.black.main,
-                        }}
-                    >
-                        Home
-                    </span>
-                </Typography>
-            </Link>
-            <Link href="/analysis" passhref="true">
-                <Typography variant="h6">
-                    <span
-                        style={{
-                            color: context.darkMode
-                                ? "#fff"
-                                : palette.black.main,
-                        }}
-                    >
-                        Analysis
-                    </span>
-                </Typography>
-            </Link>
+          >
+            Home
+          </span>
+        </Typography>
+      </Link>
+      <Link href="/analysis" passhref="true">
+        <Typography variant="h6">
+          <span
+            style={{
+              color: context.darkMode ? "#fff" : palette.black.main,
+            }}
+          >
+            Analysis
+          </span>
+        </Typography>
+      </Link>
 
-            <Link href="/projects" passhref="true">
-                <Typography variant="h6">
-                    <span
-                        style={{
-                            color: context.darkMode
-                                ? "#fff"
-                                : palette.black.main,
-                        }}
-                    >
-                        Projects
-                    </span>
-                </Typography>
-            </Link>
+      <Link href="/projects" passhref="true">
+        <Typography variant="h6">
+          <span
+            style={{
+              color: context.darkMode ? "#fff" : palette.black.main,
+            }}
+          >
+            Projects
+          </span>
+        </Typography>
+      </Link>
 
-            <Link href="/investigations" passhref="true">
-                <Typography variant="h6">
-                    <span
-                        style={{
-                            color: context.darkMode
-                                ? "#fff"
-                                : palette.black.main,
-                        }}
-                    >
-                        Investigations
-                    </span>
-                </Typography>
-            </Link>
+      <Link href="/investigations" passhref="true">
+        <Typography variant="h6">
+          <span
+            style={{
+              color: context.darkMode ? "#fff" : palette.black.main,
+            }}
+          >
+            Investigations
+          </span>
+        </Typography>
+      </Link>
 
-            <Box sx={{ flexGrow: 0 }}>
-                <Box onClick={handleOpenUserMenu}>
-                    <Typography variant="h6" passhref="true">
-                        <span
-                            style={{
-                                color: context.darkMode
-                                    ? "#fff"
-                                    : palette.black.main,
-                            }}
-                        >
-                            About
-                        </span>
-                    </Typography>
-                </Box>
-                <Menu
-                    sx={{ mt: "30px", ml: "30px" }}
-                    id="menu-appbar"
-                    anchorEl={anchorElUser}
-                    anchorOrigin={{
-                        vertical: "top",
-                        horizontal: "right",
-                    }}
-                    keepMounted
-                    transformOrigin={{
-                        vertical: "top",
-                        horizontal: "right",
-                    }}
-                    open={Boolean(anchorElUser)}
-                    onClose={handleCloseUserMenu}
-                >
-                    <MenuItem onClick={handleCloseUserMenu}>
-                        <Box
-                            sx={{
-                                color: context.darkMode
-                                    ? "#fff"
-                                    : palette.black.main,
-                                height: "20%",
-                            }}
-                        >
-                            <Link href="/mission" passhref="true">
-                                <Typography
-                                    variant="h6"
-                                    sx={{
-                                        color: context.darkMode
-                                            ? "#fff"
-                                            : palette.black.main,
-                                    }}
-                                >
-                                    Mission
-                                </Typography>
-                            </Link>
-
-                            <Link href="/bio" passhref="true">
-                                <Typography variant="h6">Bio</Typography>
-                            </Link>
-                        </Box>
-                    </MenuItem>
-                </Menu>
-            </Box>
-            <CustomCheckbox
-                icon={
-                    <NightsStayIcon
-                        color={"#F9A21B"}
-                        sx={{
-                            color: "#F9A21B !important",
-                            marginRight: "5% !important",
-                        }}
-                    />
-                }
-                checkedIcon={
-                    <WbSunnyIcon
-                        sx={{
-                            color: "#F9A21B !important",
-                            marginRight: "5% !important",
-                        }}
-                    />
-                }
-                onClick={() => {
-                    context.toggleDarkMode();
-                }}
-            />
-            <Divider orientation="vertical" flexItem />
-            <Box
-                sx={{
-                    color: context.darkMode ? "#fff" : palette.black.main,
-                    height: "30%",
-                    paddingBottom: "25px",
-                }}
+      <Box sx={{ flexGrow: 0 }}>
+        <Box onClick={handleOpenUserMenu}>
+          <Typography variant="h6" passhref="true">
+            <span
+              style={{
+                color: context.darkMode ? "#fff" : palette.black.main,
+              }}
             >
-                <Grid
-                    container
-                    flexDirection={"row"}
-                    justifyContent={"space-around"}
-                >
-                    <Grid item>
-                        <Link
-                            href="https://mobile.twitter.com/centerzomia"
-                            passhref="true"
-                        >
-                            <a target="_blank" rel="noopener noreferrer">
-                                <TwitterIcon
-                                    sx={{
-                                        color: context.darkMode
-                                            ? "#fff"
-                                            : palette.black.main,
-                                    }}
-                                />
-                            </a>
-                        </Link>
-                    </Grid>
-
-                    <Grid item>
-                        <Link
-                            href="mailto: zomiacenter@gmail.com"
-                            passhref="true"
-                        >
-                            <a target="_blank" rel="noopener noreferrer">
-                                <EmailIcon
-                                    sx={{
-                                        color: context.darkMode
-                                            ? "#fff"
-                                            : palette.black.main,
-                                    }}
-                                />
-                            </a>
-                        </Link>
-                    </Grid>
-
-                    <Grid item>
-                        <Link
-                            href="https://www.linkedin.com/company/the-zomia-center/about/?viewAsMember=true"
-                            passhref="true"
-                        >
-                            <a target="_blank" rel="noopener noreferrer">
-                                <LinkedInIcon
-                                    sx={{
-                                        color: context.darkMode
-                                            ? "#fff"
-                                            : palette.black.main,
-                                    }}
-                                />
-                            </a>
-                        </Link>
-                    </Grid>
-                </Grid>
-            </Box>
+              About
+            </span>
+          </Typography>
         </Box>
-    );
+        <Menu
+          sx={{ mt: "30px", ml: "30px" }}
+          id="menu-appbar"
+          anchorEl={anchorElUser}
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          open={Boolean(anchorElUser)}
+          onClose={handleCloseUserMenu}
+        >
+          <MenuItem onClick={handleCloseUserMenu}>
+            <Box
+              sx={{
+                color: context.darkMode ? "#fff" : palette.black.main,
+                height: "20%",
+              }}
+            >
+              <Link href="/mission" passhref="true">
+                <Typography
+                  variant="h6"
+                  sx={{
+                    color: context.darkMode ? "#fff" : palette.black.main,
+                  }}
+                >
+                  Mission
+                </Typography>
+              </Link>
+
+              <Link href="/bio" passhref="true">
+                <Typography variant="h6">Bio</Typography>
+              </Link>
+            </Box>
+          </MenuItem>
+        </Menu>
+      </Box>
+      <CustomCheckbox
+        icon={
+          <NightsStayIcon
+            color={"#F9A21B"}
+            sx={{
+              color: "#F9A21B !important",
+              marginRight: "5% !important",
+            }}
+          />
+        }
+        checkedIcon={
+          <WbSunnyIcon
+            sx={{
+              color: "#F9A21B !important",
+              marginRight: "5% !important",
+            }}
+          />
+        }
+        onClick={() => {
+          context.toggleDarkMode();
+        }}
+      />
+      <Divider orientation="vertical" flexItem />
+      <Box
+        sx={{
+          color: context.darkMode ? "#fff" : palette.black.main,
+          height: "30%",
+          paddingBottom: "25px",
+        }}
+      >
+        <Grid container flexDirection={"row"} justifyContent={"space-around"}>
+          <Grid item>
+            <Link href="https://mobile.twitter.com/centerzomia" passhref="true">
+              <a target="_blank" rel="noopener noreferrer">
+                <TwitterIcon
+                  sx={{
+                    color: context.darkMode ? "#fff" : palette.black.main,
+                  }}
+                />
+              </a>
+            </Link>
+          </Grid>
+
+          <Grid item>
+            <Link href="mailto: info@zomiacenter.com" passhref="true">
+              <a target="_blank" rel="noopener noreferrer">
+                <EmailIcon
+                  sx={{
+                    color: context.darkMode ? "#fff" : palette.black.main,
+                  }}
+                />
+              </a>
+            </Link>
+          </Grid>
+
+          <Grid item>
+            <Link
+              href="https://www.linkedin.com/company/the-zomia-center/about/?viewAsMember=true"
+              passhref="true"
+            >
+              <a target="_blank" rel="noopener noreferrer">
+                <LinkedInIcon
+                  sx={{
+                    color: context.darkMode ? "#fff" : palette.black.main,
+                  }}
+                />
+              </a>
+            </Link>
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
+  );
 };
 
 export default Tabs;

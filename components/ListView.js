e1c48df4402eb711a425bdae9a7e57975db39803
Poseidon <ardayuvarlak@outlook.com>
2022-06-24 18:02:59 -0700

    latest section margins

--- components/ListView.js
+++ components/ListView.js
@@ -3,87 +3,99 @@ import { Grid, Typography, Link } from "@mui/material";
 import Image from "next/image";
 import palette from "../theme/palette";
 import imageLoader from "../loader";
+import { isMobile } from "react-device-detect";
 
 const ListView = ({ heading, date, author, image, latest, description }) => {
-    return (
+  return (
+    <Grid
+      container
+      direction="row"
+      justifyContent="center"
+      sx={{ paddingBottom: "2rem", marginBottom: isMobile && "8rem" }}
+    >
+      <Grid
+        item
+        xs={11}
+        md={6}
+        alignSelf={"center"}
+        marginTop={"1rem"}
+        sx={{ marginBottom: isMobile && "2rem" }}
+      >
+        {image && (
+          <Image
+            src={image}
+            loader={imageLoader}
+            unoptimized
+            width={"485px"}
+            height={"385px"}
+            alt="zomia article"
+            sx={{
+              alignSelf: "center",
+            }}
+          />
+        )}
+      </Grid>
+      <Grid item xs={10} md={6}>
         <Grid
-            container
-            direction="row"
-            justifyContent="center"
-            sx={{ paddingBottom: "2rem" }}
+          container
+          direction="column"
+          justifyContent="space-evenly"
+          alignSelf={"center"}
+          sx={{ height: "100%" }}
         >
-            <Grid item xs={11} md={6} alignSelf={"center"} marginTop={"1rem"}>
-                {image && (
-                    <Image
-                        src={image}
-                        loader={imageLoader}
-                        unoptimized
-                        width={"485px"}
-                        height={"385px"}
-                        alt="zomia article"
-                        sx={{
-                            alignSelf: "center",
-                        }}
-                    />
-                )}
+          <Typography
+            variant="h4"
+            sx={{
+              ":hover": { color: palette.orange.main },
+              fontFamily: "Merriweather !important",
+              paddingTop: isMobile && "2rem",
+            }}
+          >
+            {heading}
+          </Typography>
+          <Typography
+            variant={"subtitle1"}
+            sx={{
+              fontSize: "1.2rem !important",
+              paddingTop: isMobile && "2rem",
+            }}
+          >
+            {description && description.substring(0, 400)}
+          </Typography>
+
+          <div>
+            <Grid item sx={{ height: "1.5rem !important" }}>
+              <Typography
+                align="left"
+                variant="caption"
+                sx={{ fontSize: "1rem !important" }}
+              >
+                {"by " + author}
+              </Typography>
             </Grid>
-            <Grid item xs={10} md={6}>
-                <Grid
-                    container
-                    direction="column"
-                    justifyContent="space-evenly"
-                    alignSelf={"center"}
-                    sx={{ height: "100%" }}
-                >
-                    <Typography
-                        variant="h4"
-                        sx={{
-                            ":hover": { color: palette.orange.main },
-                            fontFamily: "Merriweather !important",
-                        }}
-                    >
-                        {heading}
-                    </Typography>
-                    <Typography
-                        variant={"subtitle1"}
-                        sx={{ fontSize: "1.2rem !important" }}
-                    >
-                        {description && description.substring(0, 400)}
-                    </Typography>
 
-                    <div>
-                        <Grid item sx={{ height: "1.5rem !important" }}>
-                            <Typography
-                                align="left"
-                                variant="caption"
-                                sx={{ fontSize: "1rem !important" }}
-                            >
-                                {"by " + author}
-                            </Typography>
-                        </Grid>
+            <Grid item sx={{ height: "1.5rem !important" }}>
+              <Typography
+                align="left"
+                variant="caption"
+                sx={{ fontSize: "1rem !important" }}
+              >
+                {date}
+              </Typography>
+            </Grid>
+          </div>
 
-                        <Grid item sx={{ height: "1.5rem !important" }}>
-                            <Typography
-                                align="left"
-                                variant="caption"
-                                sx={{ fontSize: "1rem !important" }}
-                            >
-                                {date}
-                            </Typography>
-                        </Grid>
-                    </div>
-
-                    {/* <div>
+          {/* <div>
                         <Typography variant={"overline"}>
                             {"by " + author}
                         </Typography>
                         <br />
                         <Typography variant="overline">{date}</Typography>
                     </div> */}
-                </Grid>
-            </Grid>
         </Grid>
-    );
+      </Grid>
+    </Grid>
+  );
 };
 
 export default ListView;

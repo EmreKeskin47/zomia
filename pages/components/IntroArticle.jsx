dc22d84a871af8328bd0e15f99fb9aea36d4ea57
Poseidon <ardayuvarlak@outlook.com>
2022-06-24 17:45:47 -0700

    margin

--- pages/components/IntroArticle.jsx
+++ pages/components/IntroArticle.jsx
@@ -8,84 +8,75 @@ import { useRouter } from "next/router";
 import palette from "../../theme/palette";
 import singleContext from "../../SingleContext";
 import { adjustNewLines } from "../../utils/ArticleParagraph";
+import { isMobile } from "react-device-detect";
 
 export default function IntroArticle(props) {
-    const context = useContext(singleContext);
-    const router = useRouter();
-    const { data } = props;
+  const context = useContext(singleContext);
+  const router = useRouter();
+  const { data } = props;
 
-    return (
-        <Grid
-            sx={{
-                borderRadius: 0,
-                borderColor: palette.orange.main,
-                border: "10px",
-            }}
+  return (
+    <Grid
+      sx={{
+        borderRadius: 0,
+        borderColor: palette.orange.main,
+        border: "10px",
+        marginBottom: isMobile && "5rem",
+      }}
+    >
+      <Card
+        sx={{
+          maxWidth: 1,
+        }}
+      >
+        <CardActionArea
+          onClick={() => router.push(`investigations/${data.id}`)}
         >
-            <Card
-                sx={{
-                    maxWidth: 1,
-                }}
+          {data && data.image && (
+            <CardMedia component="img" image={data.image} alt="top article" />
+          )}
+          <CardContent
+            sx={{
+              backgroundColor: context.darkMode ? palette.black.main : "white",
+            }}
+          >
+            <Typography
+              gutterBottom
+              variant="h4"
+              component="div"
+              sx={{ fontFamily: "Merriweather !important" }}
             >
-                <CardActionArea
-                    onClick={() => router.push(`investigations/${data.id}`)}
-                >
-                    {data && data.image && (
-                        <CardMedia
-                            component="img"
-                            image={data.image}
-                            alt="top article"
-                        />
-                    )}
-                    <CardContent
-                        sx={{
-                            backgroundColor: context.darkMode
-                                ? palette.black.main
-                                : "white",
-                        }}
-                    >
-                        <Typography
-                            gutterBottom
-                            variant="h4"
-                            component="div"
-                            sx={{ fontFamily: "Merriweather !important" }}
-                        >
-                            {data && data.title.substring(0, 72)}
-                        </Typography>
-                        <Box sx={{ textAlign: "left", width: "100%" }}>
-                            <Typography
-                                align="left"
-                                variant="caption"
-                                sx={{ fontSize: "1rem !important" }}
-                            >
-                                {data && data.date} /
-                            </Typography>
-                            <Typography
-                                align="left"
-                                variant="caption"
-                                sx={{ fontSize: "1rem !important" }}
-                            >
-                                {data && data.author}
-                            </Typography>
-                        </Box>
-                        <hr
-                            style={{
-                                width: "100%",
-                                marginTop: "10px",
-                                marginBottom: "10px",
-                            }}
-                        />
-                        <Typography
-                            variant="body1"
-                            fontSize={"1.2rem !important"}
-                        >
-                            {data &&
-                                data.description &&
-                                data.description.substring(0, 400)}
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
-            </Card>
-        </Grid>
-    );
+              {data && data.title.substring(0, 72)}
+            </Typography>
+            <Box sx={{ textAlign: "left", width: "100%" }}>
+              <Typography
+                align="left"
+                variant="caption"
+                sx={{ fontSize: "1rem !important" }}
+              >
+                {data && data.date} /
+              </Typography>
+              <Typography
+                align="left"
+                variant="caption"
+                sx={{ fontSize: "1rem !important" }}
+              >
+                {data && data.author}
+              </Typography>
+            </Box>
+            <hr
+              style={{
+                width: "100%",
+                marginTop: "10px",
+                marginBottom: "10px",
+              }}
+            />
+            <Typography variant="body1" fontSize={"1.2rem !important"}>
+              {data && data.description && data.description.substring(0, 400)}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Grid>
+  );
 }

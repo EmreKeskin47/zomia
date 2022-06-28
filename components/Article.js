d56c664ab43c65111971b35a9f43685da1ad3df1
RuntimeTerror47 <emrekeskin47@icloud.com>
2022-06-28 19:43:01 +0300

    article link and favicon

--- components/Article.js
+++ components/Article.js
@@ -12,177 +12,200 @@ import SectionBreaker from "../pages/components/SectionBreaker";
 import imageLoader from "../loader";
 
 const Article = (props) => {
-  const { article, isReport } = props;
+    const { article, isReport } = props;
 
-  const [offset, setOffset] = useState(0);
-  useEffect(() => {
-    const onScroll = () => setOffset(window.pageYOffset);
-    // clean up code
-    window.removeEventListener("scroll", onScroll);
-    window.addEventListener("scroll", onScroll, { passive: true });
-    return () => window.removeEventListener("scroll", onScroll);
-  }, []);
+    const [offset, setOffset] = useState(0);
+    useEffect(() => {
+        const onScroll = () => setOffset(window.pageYOffset);
+        // clean up code
+        window.removeEventListener("scroll", onScroll);
+        window.addEventListener("scroll", onScroll, { passive: true });
+        return () => window.removeEventListener("scroll", onScroll);
+    }, []);
 
-  if (article) {
-    article.text = MakeArticleParagraph2(article.text);
+    if (article) {
+        article.text = MakeArticleParagraph2(article.text);
 
-    return (
-      <>
-        <AppBar
-          position="fixed"
-          sx={{
-            zIndex: 2,
-            height: {
-              xs: "65px !important",
-              md: "100px !important",
-            },
-          }}
-        >
-          <OnlyTabsBar />
-          {offset > 250 && (
-            <AppBar
-              position="relative"
-              sx={{
-                textIndent: "1rem",
-              }}
-            >
-              <Grid
-                container
-                flexDirection={"row"}
-                justifyContent={"flex-start"}
-                alignItems={"center"}
-                alignSelf={"center"}
-                alignContent={"center"}
-              >
-                <Grid item paddingTop={1}>
-                  <Image
-                    src="/static/o-logo.png"
-                    loader={imageLoader}
-                    unoptimized
-                    alt="Zomia Amblem"
-                    width="40px"
-                    height="40px"
-                  />
-                </Grid>
-                <Grid item alignSelf={"center"}>
-                  <Typography
-                    variant="h6"
-                    fontStyle={"italic"}
-                    bgcolor={"black"}
-                    fontSize={{
-                      xs: "14px !important",
-                      md: "20px !important",
-                      fontFamily: "Joan !important",
-                    }}
-                  >
-                    {article.title}
-                  </Typography>
-                </Grid>
-              </Grid>
-            </AppBar>
-          )}
-        </AppBar>
-
-        <Paper>
-          <Grid
-            container
-            justifyItems={"center"}
-            justifySelf={"center"}
-            justifyContent={"center"}
-            alignItems={"center"}
-          >
-            <Grid item xs={10} lg={9} alignSelf={"center"}>
-              <Box marginY={5}>
-                <Box display="flex" alignItems="center" marginY={5}>
-                  <VerticalDivider />
-                  <Box>
-                    <Typography
-                      variant="h3"
-                      sx={{
-                        marginY: 3,
-                        fontSize: "3.5rem !important",
-                        marginBottom: "6px !important",
-                        fontFamily: "Tiro Telugu !important",
-                      }}
-                    >
-                      {article.title}
-                    </Typography>
-                    {article.author && (
-                      <>
-                        <Typography
-                          variant="subtitle1"
-                          sx={{
-                            marginY: 3,
-                            fontFamily: "Heebo !important",
-                          }}
-                        >
-                          {"by " + article.author}
-                        </Typography>
-                        <Typography
-                          variant="subtitle1"
-                          sx={{
-                            marginY: 3,
-                            fontFamily: "Heebo !important",
-                          }}
-                        >
-                          {article.date}
-                        </Typography>
-                      </>
-                    )}
-                  </Box>
-                </Box>
-
-                {article.image && (
-                  <Box>
-                    <Image
-                      src={article.image}
-                      loader={imageLoader}
-                      unoptimized
-                      width={"1300px"}
-                      height={"750px"}
-                      alt="zomia article"
-                    />
-                  </Box>
-                )}
-
-                <Grid
-                  item
-                  xs={12}
-                  lg={11.5}
-                  justifyItems={"center"}
-                  justifyContent={"center"}
-                  alignItems={"center"}
-                  justifySelf={"center"}
-                >
-                  {article.text
-                    .split("<br/>" || "<br>")
-                    .map((paragraph, index) => {
-                      if (paragraph.includes(".") || paragraph.includes("@")) {
-                        return <RenderBody key={index} text={paragraph} />;
-                      } else {
-                        return <RenderSubtitle key={index} text={paragraph} />;
-                      }
-                    })}
-                </Grid>
-              </Box>
-            </Grid>
-          </Grid>
-          {!isReport && (
+        return (
             <>
-              <SectionBreaker text="Related Articles" link="/investigations" />
-              <CardList type="article" />
+                <AppBar
+                    position="fixed"
+                    sx={{
+                        zIndex: 2,
+                        height: {
+                            xs: "65px !important",
+                            md: "100px !important",
+                        },
+                    }}
+                >
+                    <OnlyTabsBar />
+                    {offset > 250 && (
+                        <AppBar
+                            position="relative"
+                            sx={{
+                                textIndent: "1rem",
+                            }}
+                        >
+                            <Grid
+                                container
+                                flexDirection={"row"}
+                                justifyContent={"flex-start"}
+                                alignItems={"center"}
+                                alignSelf={"center"}
+                                alignContent={"center"}
+                            >
+                                <Grid item paddingTop={1}>
+                                    <Image
+                                        src="/static/o-logo.png"
+                                        loader={imageLoader}
+                                        unoptimized
+                                        alt="Zomia Amblem"
+                                        width="40px"
+                                        height="40px"
+                                    />
+                                </Grid>
+                                <Grid item alignSelf={"center"}>
+                                    <Typography
+                                        variant="h6"
+                                        fontStyle={"italic"}
+                                        bgcolor={"black"}
+                                        fontSize={{
+                                            xs: "14px !important",
+                                            md: "20px !important",
+                                            fontFamily: "Joan !important",
+                                        }}
+                                    >
+                                        {article.title}
+                                    </Typography>
+                                </Grid>
+                            </Grid>
+                        </AppBar>
+                    )}
+                </AppBar>
+
+                <Paper>
+                    <Grid
+                        container
+                        justifyItems={"center"}
+                        justifySelf={"center"}
+                        justifyContent={"center"}
+                        alignItems={"center"}
+                    >
+                        <Grid item xs={10} lg={9} alignSelf={"center"}>
+                            <Box marginY={5}>
+                                <Box
+                                    display="flex"
+                                    alignItems="center"
+                                    marginY={5}
+                                >
+                                    <VerticalDivider />
+                                    <Box>
+                                        <Typography
+                                            variant="h3"
+                                            sx={{
+                                                marginY: 3,
+                                                fontSize: "3.5rem !important",
+                                                marginBottom: "6px !important",
+                                                fontFamily:
+                                                    "Tiro Telugu !important",
+                                            }}
+                                        >
+                                            {article.title}
+                                        </Typography>
+                                        {article.author && (
+                                            <>
+                                                <Typography
+                                                    variant="subtitle1"
+                                                    sx={{
+                                                        marginY: 3,
+                                                        fontFamily:
+                                                            "Heebo !important",
+                                                    }}
+                                                >
+                                                    {"by " + article.author}
+                                                </Typography>
+                                                <Typography
+                                                    variant="subtitle1"
+                                                    sx={{
+                                                        marginY: 3,
+                                                        fontFamily:
+                                                            "Heebo !important",
+                                                    }}
+                                                >
+                                                    {article.date}
+                                                </Typography>
+                                            </>
+                                        )}
+                                    </Box>
+                                </Box>
+
+                                {article.image && (
+                                    <Box>
+                                        <Image
+                                            src={article.image}
+                                            loader={imageLoader}
+                                            unoptimized
+                                            width={"1300px"}
+                                            height={"750px"}
+                                            alt="zomia article"
+                                        />
+                                    </Box>
+                                )}
+
+                                <Grid
+                                    item
+                                    xs={12}
+                                    lg={11.5}
+                                    justifyItems={"center"}
+                                    justifyContent={"center"}
+                                    alignItems={"center"}
+                                    justifySelf={"center"}
+                                >
+                                    {article.text
+                                        .split("<br/>" || "<br>")
+                                        .map((paragraph, index) => {
+                                            if (
+                                                paragraph.includes(".") ||
+                                                paragraph.includes("@")
+                                            ) {
+                                                return (
+                                                    <RenderBody
+                                                        key={index}
+                                                        text={paragraph}
+                                                    />
+                                                );
+                                            } else {
+                                                return (
+                                                    <RenderSubtitle
+                                                        key={index}
+                                                        text={paragraph}
+                                                    />
+                                                );
+                                            }
+                                        })}
+                                </Grid>
+                            </Box>
+                        </Grid>
+                    </Grid>
+                    {!isReport && (
+                        <>
+                            <SectionBreaker
+                                text="Related Articles"
+                                link="/analysis"
+                            />
+                            <CardList type="article" />
+                        </>
+                    )}
+                </Paper>
             </>
-          )}
-        </Paper>
-      </>
-    );
-  } else {
-    return (
-      <div>
-        <h1>Loading...</h1>
-      </div>
-    );
-  }
+        );
+    } else {
+        return (
+            <div>
+                <h1>Loading...</h1>
+            </div>
+        );
+    }
 };
 
 export default Article;
--- pages/components/CardList.jsx
+++ pages/components/CardList.jsx
@@ -2,85 +2,85 @@ import React, { useEffect, useState } from "react";
 import { Grid, Link } from "@mui/material";
 import NewsSummary from "./NewsSummary";
 import {
-  useReverseReportData,
-  useArticleData,
+    useReverseReportData,
+    useArticleData,
 } from "../../store/hooks/useData";
 
 const CardList = ({ type }) => {
-  const reportList = useReverseReportData();
-  const articleList = useArticleData();
-  const generateList = () => {
-    if (type === "report") {
-      return (
-        <Grid container direction={"row"} justifyContent={"center"}>
-          {reportList &&
-            reportList.slice(0, 3).map((item) => (
-              <Grid
-                item
-                key={item.id}
-                sx={{
-                  paddingLeft: 3,
-                  paddingRight: 3,
-                }}
-              >
-                <Link
-                  href={`/investigations/${item.id}`}
-                  sx={{ paddingLeft: 3, paddingRight: 3 }}
-                  replace="true"
-                >
-                  <NewsSummary
-                    image={item.image}
-                    title={item.title}
-                    date={item.date}
-                    description={
-                      item.description
-                        ? item.description.slice(0, 400)
-                        : "No Description"
-                    }
-                    author={item.author}
-                  />
-                </Link>
-              </Grid>
-            ))}
-        </Grid>
-      );
-    } else {
-      return (
-        <Grid container direction={"row"} justifyContent={"center"}>
-          {articleList &&
-            articleList.slice(0, 3).map((item) => (
-              <Grid
-                item
-                key={item.id}
-                sx={{
-                  paddingLeft: 3,
-                  paddingRight: 3,
-                }}
-              >
-                <Link
-                  href={`/investigations/${item.id}`}
-                  sx={{ paddingLeft: 3, paddingRight: 3 }}
-                  replace="true"
-                >
-                  <NewsSummary
-                    image={item.image}
-                    title={item.title}
-                    date={item.date}
-                    description={
-                      item.description
-                        ? item.description.slice(0, 400)
-                        : "No Description"
-                    }
-                    author={item.author}
-                  />
-                </Link>
-              </Grid>
-            ))}
-        </Grid>
-      );
-    }
-  };
-  return <Grid>{generateList()}</Grid>;
+    const reportList = useReverseReportData();
+    const articleList = useArticleData();
+    const generateList = () => {
+        if (type === "report") {
+            return (
+                <Grid container direction={"row"} justifyContent={"center"}>
+                    {reportList &&
+                        reportList.slice(0, 3).map((item) => (
+                            <Grid
+                                item
+                                key={item.id}
+                                sx={{
+                                    paddingLeft: 3,
+                                    paddingRight: 3,
+                                }}
+                            >
+                                <Link
+                                    href={`/investigations/${item.id}`}
+                                    sx={{ paddingLeft: 3, paddingRight: 3 }}
+                                    replace="true"
+                                >
+                                    <NewsSummary
+                                        image={item.image}
+                                        title={item.title}
+                                        date={item.date}
+                                        description={
+                                            item.description
+                                                ? item.description.slice(0, 400)
+                                                : "No Description"
+                                        }
+                                        author={item.author}
+                                    />
+                                </Link>
+                            </Grid>
+                        ))}
+                </Grid>
+            );
+        } else {
+            return (
+                <Grid container direction={"row"} justifyContent={"center"}>
+                    {articleList &&
+                        articleList.slice(0, 3).map((item) => (
+                            <Grid
+                                item
+                                key={item.id}
+                                sx={{
+                                    paddingLeft: 3,
+                                    paddingRight: 3,
+                                }}
+                            >
+                                <Link
+                                    href={`/analysis/${item.id}`}
+                                    sx={{ paddingLeft: 3, paddingRight: 3 }}
+                                    replace="true"
+                                >
+                                    <NewsSummary
+                                        image={item.image}
+                                        title={item.title}
+                                        date={item.date}
+                                        description={
+                                            item.description
+                                                ? item.description.slice(0, 400)
+                                                : "No Description"
+                                        }
+                                        author={item.author}
+                                    />
+                                </Link>
+                            </Grid>
+                        ))}
+                </Grid>
+            );
+        }
+    };
+    return <Grid>{generateList()}</Grid>;
 };
 
 export default CardList;

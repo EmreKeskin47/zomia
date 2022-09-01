5cbf9c8b040d1f65d5f12ea7acccdab9f621af3d
joannazomia <111094117+joannazomia@users.noreply.github.com>
2022-09-01 16:45:53 -0400

    Update Tabs.js

--- layout/Tabs.js
+++ layout/Tabs.js
@@ -178,7 +178,7 @@ const Tabs = (props) => {
           </Grid>
 
           <Grid item>
-            <Link href="mailto: info@zomiacenter.com" passhref="true">
+            <Link href="mailto: info@zomiacenter.org" passhref="true">
               <a target="_blank" rel="noopener noreferrer">
                 <FaEnvelope
                   style={{

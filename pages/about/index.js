c99ce2102b152622c0059fabb715a2f30c22a2f5
joannazomia <111094117+joannazomia@users.noreply.github.com>
2022-09-02 11:59:38 -0400

    Update index.js

--- pages/about/index.js
+++ pages/about/index.js
@@ -99,7 +99,7 @@ const About = () => {
 
                         <RenderBody
                             text={
-                                "Zomia is a 501(c)(3) nonprofit organization and all donations are tax deductible. If you would like to donate, please contact us at Link href="mailto: info@zomiacenter.org" passhref="true">info@zomiacenter.org<a target="_blank" rel="noopener noreferrer">/></a></Link>. We thank you in advance for your support."
+                                "Zomia is a 501(c)(3) nonprofit organization and all donations are tax deductible. If you would like to donate, please contact us at <Link href="mailto: info@zomiacenter.org" passhref="true">info@zomiacenter.org<a target="_blank" rel="noopener noreferrer">/></a></Link>. We thank you in advance for your support."
                             }
                         />
 

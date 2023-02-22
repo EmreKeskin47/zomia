97073c43d51b7a5a257a2ab552c6b6b3d1e9cba3
Hadas Axelbaum <hadasa@inmedmd.com>
2023-02-22 11:09:34 +0200

    add link component for quick routing

--- pages/components/ContentSummary.jsx
+++ pages/components/ContentSummary.jsx
@@ -1,37 +1,39 @@
-import { Grid, Typography, Link } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
+import ActiveLink from "./ActiveLink";
+
 import React from "react";
 
 const ContentSummary = ({ id, description, heading, date, author, last }) => {
-    return (
-        <Grid container direction="column" justifyContent="center">
-            <Grid item sx={{ paddingBottom: "1.5rem" }}>
-                <Link
-                    href={`/analysis/${id}`}
-                    underline="none"
-                    color="whitesmoke"
-                    align="left"
-                    variant="h5"
-                    sx={{
-                        fontFamily: "Joan !important",
-                    }}
-                >
-                    {heading}
-                </Link>
-            </Grid>
-            <Grid item>
-                <Typography
-                    align="left"
-                    variant="body2"
-                    sx={{ fontSize: "1rem !important" }}
-                >
-                    {description}
-                </Typography>
-            </Grid>
-            <Grid sx={{ marginTop: 3.5, marginBottom: last ? 0 : 3.5 }}>
-                <hr />
-            </Grid>
-        </Grid>
-    );
+  return (
+    <Grid container direction="column" justifyContent="center">
+      <Grid item sx={{ paddingBottom: "1.5rem" }}>
+        <ActiveLink
+          href={`/analysis/${id}`}
+          // underline="none"
+          // color="whitesmoke"
+          // align="left"
+          // variant="h5"
+          // sx={{
+          //   fontFamily: "Joan !important",
+          // }}
+        >
+          {heading}
+        </ActiveLink>
+      </Grid>
+      <Grid item>
+        <Typography
+          align="left"
+          variant="body2"
+          sx={{ fontSize: "1rem !important" }}
+        >
+          {description}
+        </Typography>
+      </Grid>
+      <Grid sx={{ marginTop: 3.5, marginBottom: last ? 0 : 3.5 }}>
+        {/* <hr /> */}
+      </Grid>
+    </Grid>
+  );
 };
 
 export default ContentSummary;

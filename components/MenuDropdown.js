1b7e1ceaf9aec62d6af22c2a9b9581af9db9f36b
Hadas Axelbaum <hadasa@inmedmd.com>
2023-02-27 23:24:08 +0200

    style dropdown background and adjust to nightmode

--- components/MenuDropdown.js
+++ components/MenuDropdown.js
@@ -1,10 +1,12 @@
-import * as React from "react";
+import React, { useContext } from "react";
 import Box from "@mui/material/Box";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import { About, Publications, OurWork } from "../models/TabEnum";
 import ActiveLink from "../pages/components/ActiveLink";
+import singleContext from "../SingleContext";
+import palette from "../theme/palette";
 
 const NavModel = {
   OurWork: [
@@ -23,12 +25,13 @@ const NavModel = {
 
 export default function MenuDropdown(props) {
   const { tabOptions } = props;
+  const context = useContext(singleContext);
 
   const listToDisplay = NavModel[tabOptions];
   const list = listToDisplay.map((item) => {
     console.log(item);
     return (
-      <ListItem disablePadding>
+      <ListItem>
         <ActiveLink href={`/new/${item}`}>
           <ListItemText primary={item} />
         </ActiveLink>
@@ -37,7 +40,13 @@ export default function MenuDropdown(props) {
   });
 
   return (
-    <Box sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+    <Box
+      sx={{
+        width: "100%",
+        maxWidth: 360,
+      }}
+      bgcolor={context.darkMode ? palette.black.main : "whitesmoke"}
+    >
       <nav aria-label="main mailbox folders">
         <List>{list}</List>
       </nav>

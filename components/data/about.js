c2e6c70700eb942398f43522619dcdfe2022460f
Hadas Axelbaum <hadasa@inmedmd.com>
2023-02-28 11:52:20 +0200

    add "our history" page

--- components/data/about.js
+++ components/data/about.js
@@ -11,6 +11,19 @@ The core strength of the Center is its alliance of researchers and specialists. 
 The Center's projects are diverse.
 `;
 
+export const OUR_HISTORY = `
+
+The Zomia Center is dedicated to addressing the needs of non-state populations worldwide.
+
+Since 2001, the number of civil wars across the globe has increased 30 percent, leading to a proliferation of ungoverned and semi-governed spaces. These include areas under the control of militias, insurgent groups, or other autonomous local actors, as well as communities affected by privatization or ethnosectarian discrimination. Such spaces are often difficult or impossible to access for state authorities and international organizations, and remain understudied. Their inhabitants meanwhile lack access to crucial legal and developmental aid, including primary health care and population-level approaches that target disease and inequality. The Zomia Center addresses this gap.  <br/>
+
+The Zomia Center partners with local organizations. The Zomia Center does not seek to replace government services, but rather collaborates with local community groups to help build sustainable and autonomous approaches to local governance. 
+
+The core strength of the Center is its alliance of researchers and specialists. Currently living and working across Syria, Yemen, Iraq, and Afghanistan, they have decades of experience in contested environments. By building and maintaining trusted networks, they ensure access to the data necessary for robust analysis, and help implement programs ranging from vaccination campaigns to legal redress for the victims of airstrikes. Researchers include physicians, journalists, anthropologists, and ethnographers, collaborating under the guidance of acclaimed sociologists and political theorists. 
+
+The Center's projects are diverse.
+`;
+
 export const bulletList1 = `Public health surveillance in Syria to assist the design of vaccination campaigns, including Covid-19 <br/>
 Programs to help victims of U.S. airstrikes navigate legal hurdles and obtain condolence payments from the U.S. government <br/>
 Projects designed to support local organizations catalog individuals forcibly disappeared by ISIS, with the aim of helping to bring closure to loved ones <br/>
---
+++ pages/Our%20history/index.js
@@ -0,0 +1,97 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+import { Box, Grid, Paper } from "@mui/material";
+import {
+  bulletList1,
+  bulletList2,
+  OUR_HISTORY,
+} from "../../components/data/about";
+import { MakeArticleParagraph2 } from "../../utils/ArticleParagraph";
+import RenderTitle from "../../components/RenderTitle";
+import RenderBody from "../../components/RenderBody";
+import RenderBulletPoint from "../../components/RenderBulletPoint";
+import RenderSubtitle from "../../components/RenderSubtitle";
+// import Partners from "../../components/Partners";
+
+const OurHistory = () => {
+  const about = MakeArticleParagraph2(OUR_HISTORY);
+  const bullet1 = MakeArticleParagraph2(bulletList1);
+  const bullet2 = MakeArticleParagraph2(bulletList2);
+
+  return (
+    <Paper sx={{ paddingTop: 5 }}>
+      <Helmet>
+        <title>Zomia Center Our History</title>
+      </Helmet>
+      <Grid
+        container
+        justifyItems={"center"}
+        justifySelf={"center"}
+        justifyContent={"center"}
+        alignItems={"center"}
+      >
+        <Grid item xs={10} lg={9} alignSelf={"center"}>
+          <Box marginY={5} sx={{ paddingBottom: "125px" }}>
+            <Box sx={{ marginLeft: { md: 2.5 } }}>
+              <RenderTitle
+                text="Our History"
+                height="100px"
+                onlyLogo={true}
+                oswald={true}
+              />
+            </Box>
+
+            <Grid
+              item
+              xs={12}
+              lg={12}
+              justifyItems={"center"}
+              justifyContent={"center"}
+              alignItems={"center"}
+              justifySelf={"center"}
+            >
+              {about.split("<br/>").map((paragraph, index) => {
+                return <RenderBody key={index} text={paragraph} />;
+              })}
+            </Grid>
+            <RenderSubtitle text="Current Initiatives" oswald={true} />
+
+            <Box marginBottom={10}>
+              {bullet1.split("<br/>").map((paragraph, index) => {
+                return <RenderBulletPoint key={index} text={paragraph} />;
+              })}
+            </Box>
+            <RenderBody
+              text={
+                "The Center is unique in linking these endeavors to cutting-edge, granular research in non-state spaces. The Center produces deeply researched reports for the public on issues ranging from the Syrian war to vaccination deserts in the United States. "
+              }
+            />
+            <RenderSubtitle text="Current Themes" oswald={true} />
+            <Box marginBottom={10}>
+              {bullet2.split("<br/>").map((paragraph, index) => {
+                return <RenderBulletPoint key={index} text={paragraph} />;
+              })}
+            </Box>
+            <RenderBody
+              text={
+                "The Center seeks to encourage the free exchange of information among the scholarly and humanitarian communities. Named for the historically contested uplands of Southeast Asia, The Zomia Center aims to build on a rich tradition of non-state scholarship and activism, in the belief that empirical understanding of the contested present is necessary for a more peaceful and egalitarian future."
+              }
+            />
+
+            <Box sx={{ marginLeft: { md: 2.5 } }}>
+              <RenderTitle text="Support Zomia" height="100px" oswald={true} />
+            </Box>
+
+            <RenderBody
+              text={
+                "Zomia is a 501(c)(3) nonprofit organization registered in the United States under EIN: 30-1201889, and all donations are tax deductible. If you would like to donate, please contact us at info@zomiacenter.org. We thank you in advance for your support."
+              }
+            />
+          </Box>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+};
+
+export default OurHistory;

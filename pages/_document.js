ccfc2757c135b60cbd18356089ba4920670a0a0c
Hadas Axelbaum <hadasa@inmedmd.com>
2023-03-07 17:06:22 +0200

    replace meta tag for google verification

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 // pages/_document.js
 
-import Document, { Html, Head, Main, NextScript} from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
@@ -18,6 +18,11 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Cantata+One&family=Fira+Sans&family=Heebo&family=Joan&family=Josefin+Sans&family=Kanit&family=Merriweather&family=Montserrat&family=Noto+Sans&family=Oswald:wght@500&family=PT+Serif&family=Poppins&family=Raleway&family=Roboto&family=Rubik&family=Tiro+Gurmukhi&family=Tiro+Telugu&family=Ubuntu&display=swap"
             rel="stylesheet"
           />
+          <meta
+            name="google-site-verification"
+            content="cuDQRQhxPo64IWgmg4D3_T8DMlFcXr7CX6bpoIjFrYo"
+          />
+          +
         </Head>
         <body>
           <Main />

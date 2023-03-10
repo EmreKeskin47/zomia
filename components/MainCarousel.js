6b2f7383ae706e581302508700559a007ee68cde
Hadas Axelbaum <hadasa@inmedmd.com>
2023-03-10 10:08:24 +0200

    set size for img in carousel

--- components/MainCarousel.js
+++ components/MainCarousel.js
@@ -3,6 +3,8 @@ import Carousel from "react-material-ui-carousel";
 import { Paper, Button } from "@mui/material";
 import { useArticleData } from "../store/hooks/useData";
 import Typography from "@mui/material/Typography";
+import Image from "next/image";
+import ActiveLink from "../pages/components/ActiveLink";
 
 export function MainCarousel(props) {
   const articleList = useArticleData();
@@ -19,15 +21,26 @@ function Item(props) {
   return (
     <Paper
       className="main-carousel"
-      sx={{
-        backgroundImage: `url(${props.item.image})`,
-        backgroundSize: "auto",
-      }}
+      //   sx={{
+      //     backgroundImage: `url(${props.item.image})`,
+      //     backgroundSize: "auto",
+      //   }}
     >
-      <Typography className="carousel-text">
-        <h2>{props.item.title}</h2>
-        <p>{props.item.description}</p>
-      </Typography>
+      <ActiveLink href={`Articles/${props.item.id}`}>
+        <Image
+          src={props.item.image}
+          // loader={imageLoader}
+          unoptimized
+          alt="Zomia Amblem"
+          width="1500rem"
+          height="600rem"
+        />
+
+        <Typography className="carousel-text">
+          <h2>{props.item.title}</h2>
+          <p>{props.item.description}</p>
+        </Typography>
+      </ActiveLink>
 
       <Button className="CheckButton">Check it out!</Button>
     </Paper>

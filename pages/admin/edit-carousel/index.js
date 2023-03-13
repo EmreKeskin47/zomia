82d04de6ecf31e55ce499b4ecd78e0c11260982a
Hadas Axelbaum <hadasa@inmedmd.com>
2023-03-13 12:34:18 +0200

    delete list from edit-carousel page

--- pages/admin/edit-carousel/index.js
+++ pages/admin/edit-carousel/index.js
@@ -8,101 +8,29 @@ import { ToastContainer, toast } from "react-toastify";
 import AdminAppBar from "../../../components/admin/AdminAppBar";
 import { useCarouselData } from "../../../store/hooks/useData";
 import { set } from "lodash";
+import { fetchCarouselPosts } from "../../../store/actions/carousel-actions";
+import * as carouselActions from "../../../store/actions/carousel-actions";
 
-// fake data generator
-const getItems = (count, offset = 0) =>
-  Array.from({ length: count }, (v, k) => k).map((k) => ({
-    id: `item-${k + offset}-${new Date().getTime()}`,
-    content: `item ${k + offset}`,
-  }));
+function EditCarousel(props) {
+  const postList = useCarouselData();
+  // const fetch = async () => {
+  //   const posts = await fetchCarousel();
+  console.log("<<<<<<<<<<<<", postList);
+  // };
 
-const reorder = (list, startIndex, endIndex) => {
-  const result = Array.from(list);
-  const [removed] = result.splice(startIndex, 1);
-  result.splice(endIndex, 0, removed);
+  // useEffect(() => {
+  //
+  // let statePosts;
 
-  return result;
-};
-
-/**
- * Moves an item from one list to another list.
- */
-const move = (source, destination, droppableSource, droppableDestination) => {
-  const sourceClone = Array.from(source);
-  const destClone = Array.from(destination);
-  const [removed] = sourceClone.splice(droppableSource.index, 1);
-
-  destClone.splice(droppableDestination.index, 0, removed);
-
-  const result = {};
-  result[droppableSource.droppableId] = sourceClone;
-  result[droppableDestination.droppableId] = destClone;
-
-  return result;
-};
-const grid = 8;
-
-const getItemStyle = (isDragging, draggableStyle) => ({
-  // some basic styles to make the items look a bit nicer
-  userSelect: "none",
-  padding: grid * 2,
-  margin: `0 0 ${grid}px 0`,
-
-  // change background colour if dragging
-  background: isDragging ? "lightgreen" : "grey",
-
-  // styles we need to apply on draggables
-  ...draggableStyle,
-});
-const getListStyle = (isDraggingOver) => ({
-  background: isDraggingOver ? "lightblue" : "lightgrey",
-  padding: grid,
-  width: 700,
-});
-
-function EditCarousel() {
-  const [state, setState] = useState([getItems(10)]);
-  const posts = useCarouselData();
-
-  useEffect(() => {
-    // console.log(posts);
-
-    let statePosts;
-
-    if (posts && posts !== []) {
-      statePosts = posts.map((post) => ({
-        id: post.id,
-        content: post.title,
-      }));
-    }
-    setState([statePosts]);
-    statePosts && console.log(statePosts);
-  }, [posts]);
-
-  function onDragEnd(result) {
-    const { source, destination } = result;
-
-    // dropped outside the list
-    if (!destination) {
-      return;
-    }
-    const sInd = +source.droppableId;
-    const dInd = +destination.droppableId;
-
-    if (sInd === dInd) {
-      const items = reorder(state[sInd], source.index, destination.index);
-      const newState = [...state];
-      newState[sInd] = items;
-      setState(newState);
-    } else {
-      const result = move(state[sInd], state[dInd], source, destination);
-      const newState = [...state];
-      newState[sInd] = result[sInd];
-      newState[dInd] = result[dInd];
-
-      setState(newState.filter((group) => group.length));
-    }
-  }
+  // if (posts && posts !== []) {
+  //   statePosts = posts.map((post) => ({
+  //     id: post.id,
+  //     content: post.title,
+  //   }));
+  // }
+  // setState([statePosts]);
+  // statePosts && console.log(statePosts);
+  // }, [state.carouselStore.posts]);
 
   return (
     <>
@@ -117,82 +45,6 @@ function EditCarousel() {
         <AdminAppBar />
 
         <Grid container direction={"column"} width={"90%"} marginX={"5%"}>
-          <div>
-            {/* <button
-              type="button"
-              onClick={() => {
-                setState([...state, []]);
-              }}
-            >
-              Add new group
-            </button>
-            <button
-              type="button"
-              onClick={() => {
-                setState([...state, getItems(1)]);
-              }}
-            >
-              Add new item
-            </button> */}
-            <div style={{ display: "flex" }}>
-              <DragDropContext onDragEnd={onDragEnd}>
-                {state.map((el, ind) => (
-                  <Droppable key={ind} droppableId={`${ind}`}>
-                    {(provided, snapshot) => (
-                      <div
-                        ref={provided.innerRef}
-                        style={getListStyle(snapshot.isDraggingOver)}
-                        {...provided.droppableProps}
-                      >
-                        {console.log("<<<<<<<<<<<<", state)}
-                        {el.map((item, index) => (
-                          <Draggable
-                            key={item.id}
-                            draggableId={item.id}
-                            index={index}
-                          >
-                            {(provided, snapshot) => (
-                              <div
-                                ref={provided.innerRef}
-                                {...provided.draggableProps}
-                                {...provided.dragHandleProps}
-                                style={getItemStyle(
-                                  snapshot.isDragging,
-                                  provided.draggableProps.style
-                                )}
-                              >
-                                <div
-                                  style={{
-                                    display: "flex",
-                                    justifyContent: "space-around",
-                                  }}
-                                >
-                                  {item.content}
-                                  <button
-                                    type="button"
-                                    onClick={() => {
-                                      const newState = [...state];
-                                      newState[ind].splice(index, 1);
-                                      setState(
-                                        newState.filter((group) => group.length)
-                                      );
-                                    }}
-                                  >
-                                    delete
-                                  </button>
-                                </div>
-                              </div>
-                            )}
-                          </Draggable>
-                        ))}
-                        {provided.placeholder}
-                      </div>
-                    )}
-                  </Droppable>
-                ))}
-              </DragDropContext>
-            </div>
-          </div>
           <ToastContainer />
         </Grid>
       </Box>
@@ -200,33 +52,17 @@ function EditCarousel() {
   );
 }
 
-// const rootElement = document.getElementById("root");
-// ReactDOM.render(<QuoteApp />, rootElement);
-
-// import React, { useState } from "react";
-
-// import "react-toastify/dist/ReactToastify.css";
-//
-
-// const EditCarousel = (props) => {
-//
-
-//   return (
-//
-
-//   );
-// };
-
 const mapStateToProps = (state) => {
+  console.log("state", state.carouselStore.posts);
   return {
     posts: state.carouselStore.posts,
   };
 };
 
-// const mapDispatchToProps = (dispatch) => {
-//   return {
-//     // fetchCa: () => dispatch(articleActions.fetchArticles()),
-//   };
-// };
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchCarousel: () => dispatch(carouselActions.fetchCarouselPosts()),
+  };
+};
 
-export default connect(mapStateToProps)(EditCarousel);
+export default connect(mapStateToProps, mapDispatchToProps)(EditCarousel);

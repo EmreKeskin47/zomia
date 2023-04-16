12f278600b299238df8339a7aa4994252fb0364c
Hadas Axelbaum <hadasa@inmedmd.com>
2023-04-15 22:58:07 -0500

    more styling carousel and partens section

--- components/OurPartners.js
+++ components/OurPartners.js
@@ -3,10 +3,12 @@ import {
   Container,
   Stack,
   Button,
+  Grid,
   Divider,
   Typography,
 } from "@mui/material";
 import Image from "next/image";
+import { Partners } from "../models/Partners";
 
 const OurPartners = () => {
   return (
@@ -17,97 +19,26 @@ const OurPartners = () => {
           <h2>Our Partners</h2>
         </Typography>
       </Divider>
-      <Stack direction="row" spacing={6} justifyContent="center">
-        <Box>
-          <Image
-            src={"/partners/arizona-state-university.jpg"}
-            unoptimized
-            width={"120px"}
-            height={"80px"}
-            objectFit={"cover"}
-            alt="arizona state university"
-          ></Image>
-        </Box>
-        <Box>
-          <Image
-            src={"/partners/american-university-of-afghanistan.png"}
-            unoptimized
-            width={"120px"}
-            height={"80px"}
-            objectFit={"cover"}
-            alt="american university of afghanistan"
-          ></Image>
-        </Box>
-        <Box>
-          {" "}
-          <Image
-            src={"/partners/code-to-inspire.png"}
-            unoptimized
-            width={"120px"}
-            height={"80px"}
-            objectFit={"cover"}
-            alt="code to inspire"
-          ></Image>
-        </Box>
-
-        <Box>
-          {" "}
-          <Image
-            src={"/partners/The-Trade-Desk.png"}
-            unoptimized
-            width={"280px"}
-            height={"80px"}
-            objectFit={"cover"}
-            alt="the trade desk"
-          ></Image>
-        </Box>
-
-        <Box>
-          {" "}
-          <Image
-            src={"/partners/International_Rescue_Committee.png"}
-            unoptimized
-            width={"280px"}
-            height={"80px"}
-            objectFit={"cover"}
-            alt="International Rescue Committee"
-          ></Image>
-        </Box>
-
-        <Box>
-          {" "}
-          <Image
-            src={"/partners/EMM.png"}
-            unoptimized
-            width={"120px"}
-            height={"80px"}
-            objectFit={"cover"}
-            alt="Episcopal Migration Ministries"
-          ></Image>
-        </Box>
-        <Box>
-          {" "}
-          <Image
-            src={"/partners/LIRS.jpg"}
-            unoptimized
-            width={"280px"}
-            height={"80px"}
-            objectFit={"cover"}
-            alt="Episcopal Migration Ministries"
-          ></Image>
-        </Box>
-        <Box>
-          {" "}
-          <Image
-            src={"/partners/HIAS.png"}
-            unoptimized
-            width={"150px"}
-            height={"80px"}
-            objectFit={"cover"}
-            alt="Episcopal Migration Ministries"
-          ></Image>
-        </Box>
-      </Stack>
+      <Grid container direction={"row"} justifyContent={"center"}>
+        {Partners.map((partner) => (
+          <Grid
+            sx={{
+              paddingX: 1,
+              paddingY: 1,
+            }}
+            key={partner.img}
+          >
+            <Image
+              src={`/partners/${partner.img}`}
+              unoptimized
+              width={partner.width}
+              height={partner.height}
+              objectFit={"cover"}
+              alt="arizona state university"
+            ></Image>
+          </Grid>
+        ))}
+      </Grid>
     </>
   );
 };
---
+++ models/Partners.js
@@ -0,0 +1,15 @@
+export const Partners = [
+  { img: "code-to-inspire.png", width: "120px", height: "80px" },
+
+  { img: "International_Rescue_Committee.png", width: "280px", height: "80px" },
+  { img: "EMM.png", width: "120px", height: "80px" },
+  { img: "LIRS.jpg", width: "280px", height: "80px" },
+  { img: "arizona-state-university.jpg", width: "120px", height: "80px" },
+  {
+    img: "american-university-of-afghanistan.png",
+    width: "120px",
+    height: "80px",
+  },
+  { img: "The-Trade-Desk.png", width: "280px", height: "80px" },
+  { img: "HIAS.png", width: "150px", height: "80px" },
+];
